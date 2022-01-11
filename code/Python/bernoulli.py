7788cce9f465e223e88a94ef0b3e21668b0d5603
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2022-01-11 19:53:11 +0900

    tweak

--- code/Python/bernoulli.py
+++ code/Python/bernoulli.py
@@ -8,7 +8,7 @@ from matplotlib.animation import FuncAnimation
 
 #%%
 
-## 確率の計算
+### 確率の計算
 
 # パラメータを指定
 phi = 0.3
@@ -26,36 +26,36 @@ prob = phi**x * (1.0 - phi)**(1.0 - x)
 print(prob)
 
 # 対数をとった定義式により確率を計算
-ln_prob = x * np.log(phi) + (1.0 - x) * np.log(1.0 - phi)
-prob = np.exp(ln_prob)
-print(prob, ln_prob)
+log_prob = x * np.log(phi) + (1.0 - x) * np.log(1.0 - phi)
+prob = np.exp(log_prob)
+print(prob, log_prob)
 
 # ベルヌーイ分布の関数により確率を計算
 prob = bernoulli.pmf(k=x, p=phi)
 print(prob)
 
 # ベルヌーイ分布の対数をとった関数により確率を計算
-ln_prob = bernoulli.logpmf(k=x, p=phi)
-prob = np.exp(ln_prob)
-print(prob, ln_prob)
+log_prob = bernoulli.logpmf(k=x, p=phi)
+prob = np.exp(log_prob)
+print(prob, log_prob)
 
 # 二項分布の関数により確率を計算
 prob = binom.pmf(k=x, n=1, p=phi)
 print(prob)
 
 # 二項分布の対数をとった関数により確率を計算
-ln_prob = binom.logpmf(k=x, n=1, p=phi)
-prob = np.exp(ln_prob)
-print(prob, ln_prob)
+log_prob = binom.logpmf(k=x, n=1, p=phi)
+prob = np.exp(log_prob)
+print(prob, log_prob)
 
 # 多項分布の関数により確率を計算
 prob = multinomial.pmf(x=x_v, n=1, p=phi_v)
 print(prob)
 
 # 多項分布の対数をとった関数により確率を計算
-ln_prob = multinomial.logpmf(x=x_v, n=1, p=phi_v)
-prob = np.exp(ln_prob)
-print(prob, ln_prob)
+log_prob = multinomial.logpmf(x=x_v, n=1, p=phi_v)
+prob = np.exp(log_prob)
+print(prob, log_prob)
 
 # インデックスにより確率を抽出
 prob = phi_v[np.int(x)]
@@ -63,10 +63,11 @@ print(prob)
 
 #%%
 
-## 統計量の計算
+### 統計量の計算
 
 # パラメータを指定
 phi = 0.3
+
 
 # 定義式により平均を計算
 E_x = phi
@@ -90,18 +91,20 @@ print(binom.var(n=1, p=phi))
 
 #%%
 
-## グラフの作成
+### グラフの作成
 
 # パラメータを指定
 phi = 0.3
 
-# 作図用の値を作成
+# 作図用のxの値を作成
 x_vals = np.array([0.0, 1.0])
+
+# 確率を計算
 probability = np.array([1.0 - phi, phi])
 
 # ベルヌーイ分布を作図
 plt.figure(figsize=(9, 8)) # 図の設定
-plt.bar(x_vals, probability, color='#00A968') # 棒グラフ
+plt.bar(x=x_vals, height=probability, color='#00A968') # 棒グラフ
 #plt.vlines(x=E_x, ymin=0.0, ymax=np.max(probability), color='orange', linestyle='--', label='$E[x]$') # 平均
 #plt.vlines(x=E_x - V_x, ymin=0.0, ymax=np.max(probability), color='orange', linestyle=':', label='$E[x] - \sqrt{V[x]}$') # 平均 - 標準偏差
 #plt.vlines(x=E_x + V_x, ymin=0.0, ymax=np.max(probability), color='orange', linestyle=':', label='$E[x] + \sqrt{V[x]}$') # 平均 + 標準偏差
@@ -109,14 +112,14 @@ plt.xlabel('x') # x軸ラベル
 plt.ylabel('probability') # y軸ラベル
 plt.suptitle('Bernoulli Distribution', fontsize=20) # 図タイトル
 plt.title('$\phi=' + str(phi) + '$', loc='left') # タイトル
-plt.xticks(ticks=[0, 1]) # x軸目盛
+plt.xticks(ticks=x_vals) # x軸目盛
 #plt.legend() # 凡例
 plt.grid() # グリッド線
 plt.show() # 描画
 
 #%%
 
-## パラメータとグラフの形状の関係
+### パラメータと分布の形状の関係
 
 # 作図用のphiの値を作成
 phi_vals = np.arange(start=0.0, stop=1.01, step=0.01)
@@ -133,7 +136,7 @@ def update(i):
     phi = phi_vals[i]
     
     # ベルヌーイ分布を作図
-    plt.bar([0.0, 1.0], [1.0 - phi, phi], color='#00A968') # 棒グラフ
+    plt.bar(x=[0.0, 1.0], height=[1.0 - phi, phi], color='#00A968') # 棒グラフ
     plt.xlabel('x') # x軸ラベル
     plt.ylabel('probability') # y軸ラベル
     plt.suptitle('Bernoulli Distribution', fontsize=20) # 図タイトル
@@ -170,26 +173,26 @@ frequency = np.array([np.sum(x_n == 0), np.sum(x_n == 1)])
 
 # サンプルのヒストグラムを作成
 plt.figure(figsize=(9, 8)) # 図の設定
-plt.bar(x_vals, frequency, color='#00A968') # ヒストグラム
+plt.bar(x=x_vals, height=frequency, color='#00A968') # ヒストグラム
 plt.xlabel('x') # x軸ラベル
 plt.ylabel('frequency') # y軸ラベル
 plt.suptitle('Bernoulli Distribution', fontsize=20) # 図タイトル
 plt.title('$\phi=' + str(phi) + ', N=' + str(N) + 
           '=(' + str(np.sum(x_n == 0)) + ', ' + str(np.sum(x_n == 1)) + ')$', loc='left') # タイトル
-plt.xticks(ticks=[0, 1]) # x軸目盛
+plt.xticks(ticks=x_vals) # x軸目盛
 plt.grid() # グリッド線
 plt.show() # 描画
 
 # サンプルの構成比を作図
 plt.figure(figsize=(9, 8)) # 図の設定
-plt.bar(x_vals, probability, color='white', edgecolor='green', linestyle='--') # 分布
-plt.bar(x_vals, frequency / N, color='#00A968', alpha=0.8) # 構成比
+plt.bar(x=x_vals, height=probability, color='white', edgecolor='green', linestyle='--') # 分布
+plt.bar(x=x_vals, height=frequency / N, color='#00A968', alpha=0.8) # 構成比
 plt.xlabel('x') # x軸ラベル
 plt.ylabel('proportion') # y軸ラベル
 plt.suptitle('Bernoulli Distribution', fontsize=20) # 図タイトル
 plt.title('$\phi=' + str(phi) + ', N=' + str(N) + 
           '=(' + str(np.sum(x_n == 0)) + ', ' + str(np.sum(x_n == 1)) + ')$', loc='left') # タイトル
-plt.xticks(ticks=[0, 1]) # x軸目盛
+plt.xticks(ticks=x_vals) # x軸目盛
 plt.grid() # グリッド線
 plt.show() # 描画
 
@@ -215,14 +218,14 @@ def update(n):
     frequency = np.array([np.sum(x_n[:(n+1)] == 0), np.sum(x_n[:(n+1)] == 1)])
     
     # サンプルのヒストグラムを作成
-    plt.bar(x_vals, frequency, color='#00A968', zorder=1) # ヒストグラム
-    plt.scatter(x_n[n], 0.0, color='orange', s=100, zorder=2) # サンプル
+    plt.bar(x=x_vals, height=frequency, color='#00A968', zorder=1) # ヒストグラム
+    plt.scatter(x=x_n[n], y=0.0, color='orange', s=100, zorder=2) # サンプル
     plt.xlabel('x') # x軸ラベル
     plt.ylabel('frequency') # y軸ラベル
     plt.suptitle('Bernoulli Distribution', fontsize=20) # 図タイトル
     plt.title('$\phi=' + str(phi) + ', N=' + str(n) + 
               '=(' + str(frequency[0]) + ', ' + str(frequency[1]) + ')$', loc='left') # タイトル
-    plt.xticks(ticks=[0, 1]) # x軸目盛
+    plt.xticks(ticks=x_vals) # x軸目盛
     plt.grid() # グリッド線
     plt.ylim(-1.0, y_max + 1.0) # y軸の表示範囲
 
@@ -246,15 +249,15 @@ def update(n):
     frequency = np.array([np.sum(x_n[:(n+1)] == 0), np.sum(x_n[:(n+1)] == 1)])
     
     # サンプルの構成比を作成
-    plt.bar(x_vals, probability, color='white', edgecolor='green', linestyle='--', zorder=1) # 分布
-    plt.bar(x_vals, frequency / (n + 1), color='#00A968', alpha=0.8, zorder=2) # 構成比
-    plt.scatter(x_n[n], 0.0, color='orange', s=100, zorder=3) # サンプル
+    plt.bar(x=x_vals, height=probability, color='white', edgecolor='green', linestyle='--', zorder=1) # 分布
+    plt.bar(x=x_vals, height=frequency / (n + 1), color='#00A968', alpha=0.8, zorder=2) # 構成比
+    plt.scatter(x=x_n[n], y=0.0, color='orange', s=100, zorder=3) # サンプル
     plt.xlabel('x') # x軸ラベル
     plt.ylabel('proportion') # y軸ラベル
     plt.suptitle('Bernoulli Distribution', fontsize=20) # 図タイトル
     plt.title('$\phi=' + str(phi) + ', N=' + str(n) + 
               '=(' + str(frequency[0]) + ', ' + str(frequency[1]) + ')$', loc='left') # タイトル
-    plt.xticks(ticks=[0, 1]) # x軸目盛
+    plt.xticks(ticks=x_vals) # x軸目盛
     plt.grid() # グリッド線
     plt.ylim(-0.1, 1.1) # y軸の表示範囲
 
