ca3aeb7ec621b8d3dc0cf71cfc5acfc6d92e4d08
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2025-07-17 12:06:04 +0900

    「乱数の可視化」をアプデ

--- code/binomial/random_number.R
+++ code/binomial/random_number.R
@@ -14,166 +14,380 @@ library(gganimate)
 library(ggplot2)
 
 
-# 乱数の生成 -------------------------------------------------------------------
+# 乱数と生成分布の関係 ---------------------------------------------------------
 
-### ・サンプリング -----
-
-# パラメータを指定
-phi <- 0.35
+### パラメータの設定 -----
 
 # 試行回数を指定
-M <- 10
+M <- 9
 
-# データ数を指定
-N <- 1000
+# パラメータを指定
+phi <- 0.4
 
 
-# 二項分布に従う乱数を生成
+### 乱数の生成 -----
+
+# サンプルサイズを指定
+N <- 3000
+
+# 二項分布の乱数を生成
 x_n <- rbinom(n = N, size = M, prob = phi)
 
 
-### ・乱数の可視化 -----
+# x軸の範囲を設定
+x_max <- M
 
-# xがとり得る値を作成
-x_vals <- 0:M
-
-# サンプルを集計
-freq_df <- tidyr::tibble(x = x_n) |> # 乱数を格納
-  dplyr::count(x, name = "frequency") |> # 度数を集計
-  dplyr::right_join(tidyr::tibble(x = x_vals), by = "x") |> # 全てのパターンに追加
-  dplyr::mutate(frequency = tidyr::replace_na(frequency, 0)) # サンプルにない場合の欠損値を0に置換
+# x軸の値を作成
+x_vec <- seq(from = 0, to = x_max, by = 1)
 
 # 二項分布を計算
 prob_df <- tidyr::tibble(
-  x = x_vals, # 確率変数
-  probability = dbinom(x = x_vals, size = M, prob = phi) # 確率
+  x    = x_vec, # 確率変数
+  prob = dbinom(x = x, size = M, prob = phi) # 確率
 )
 
 
-# サンプルのヒストグラムを作成:度数
-ggplot(data = freq_df, mapping = aes(x = x, y = frequency)) + # データ
-  geom_bar(stat = "identity", fill = "#00A968") + # 度数
-  scale_x_continuous(breaks = x_vals, minor_breaks = FALSE) + # x軸目盛
-  labs(title = "Binomial Distribution", 
-       subtitle = paste0("phi=", phi, ", M=", M, ", N=", N), 
-       x = "x", y = "frequency") # ラベル
+### 乱数の可視化 -----
 
-# サンプルのヒストグラムを作成:相対度数
-ggplot() + 
-  geom_bar(data = freq_df, mapping = aes(x = x, y = frequency/N), 
-           stat = "identity", fill = "#00A968") + # 相対度数
-  geom_bar(data = prob_df, mapping = aes(x = x, y = probability), 
-           stat = "identity", alpha = 0, color = "darkgreen", linetype = "dashed") + # 元の分布
-  scale_x_continuous(breaks = x_vals, minor_breaks = FALSE) + # x軸目盛
-  labs(
-    title = "Binomial Distribution", 
-    subtitle = parse(
-      text = paste0(
-        "list(phi==", phi, ", M==", M, ", N==", "(list(", paste0(freq_df[["frequency"]], collapse = ", "), "))", ")")
-    ), 
-    x = "x", y = "relative frequency"
-  ) # ラベル
+#### 1サンプルずつ集計 -----
 
+# フレーム数を設定
+frame_num <- N
+frame_num <- 300
 
-# 乱数と分布の関係：アニメーションによる可視化 --------------------------------------------------
-
-# パラメータを指定
-phi <- 0.35
-
-# 試行回数を指定
-M <- 10
-
-# データ数(フレーム数)を指定
-N <- 300
-
-
-# 二項分布に従う乱数を生成
-x_n <- rbinom(n = N, size = M, prob = phi)
-
-# xがとり得る値を作成
-x_vals <- 0:M
-
-# サンプルを集計
-freq_df <- tibble::tibble(
-  x = x_n, # サンプル
-  n = 1:N, # データ番号
-  frequency = 1 # 集計用の値
-) |> 
-  dplyr::right_join(tidyr::expand_grid(x = x_vals, n = 1:N), by = c("x", "n")) |> # 全てのパターンに結合
-  dplyr::mutate(frequency = tidyr::replace_na(frequency, replace = 0)) |> # サンプルにない場合の欠損値を0に置換
-  dplyr::arrange(n, x) |> # 集計用に昇順に並べ替え
-  dplyr::group_by(x) |> # 集計用にグループ化
-  dplyr::mutate(frequency = cumsum(frequency)) |> # 累積和を計算
-  dplyr::ungroup() # グループ化を解除
-
-# フレーム切替用のラベルを作成
-label_vec <- freq_df |> 
-  tidyr::pivot_wider(
-    id_cols = n, 
-    names_from = x, 
-    names_prefix = "x", 
-    values_from = frequency
-  ) |> # 度数列を展開
-  tidyr::unite(col = "label", dplyr::starts_with("x"), sep = ", ") |> # 度数情報をまとめて文字列化
-  dplyr::mutate(
-    label = paste0("phi=", phi, ", M=", M, ", N=", n, "=(", label, ")") %>% 
-      factor(., levels = .)
-  ) |> # パラメータ情報をまとめて因子化
-  dplyr::pull(label) # ベクトルとして取得
-
-# フレーム切替用のラベルを追加
-anime_freq_df <- freq_df |> 
-  tibble::add_column(parameter = rep(label_vec, each = M+1))
 
 # サンプルを格納
-anime_data_df <- tibble::tibble(
-  x = x_n, # サンプル
-  parameter = label_vec # フレーム切替用ラベルを追加
+anim_sample_df <- tibble::tibble(
+  frame_i = 1:frame_num, # フレーム番号
+  n = 1:frame_num, # サンプル番号
+  x = x_n[n]       # サンプル値
 )
 
-# 二項分布の情報を複製
-anime_prob_df <- tibble::tibble(
-  x = x_vals, # 確率変数
-  probability = dbinom(x = x_vals, size = M, prob = phi), # 確率
-  num = N # 複製数
+# サンプルを集計
+anim_freq_df <- tibble::tibble(
+  frame_i = 1:frame_num, # フレーム番号
+  N       = 1:frame_num  # サンプルサイズ
 ) |> 
-  tidyr::uncount(num) |> # データ数分に複製
-  tibble::add_column(parameter = rep(label_vec, times = length(x_vals))) |> # フレーム切替用ラベルを追加
-  dplyr::arrange(parameter) # サンプリング回数ごとに並べ替え
+  dplyr::reframe(
+    n = 1:N, # サンプル番号
+    .by = c(frame_i, N)
+  ) |> 
+  dplyr::mutate(
+    x = x_n[n] # サンプル値
+  ) |> 
+  dplyr::count(
+    frame_i, N, x, name = "freq" # 度数
+  ) |> 
+  dplyr::mutate(
+    rel_freq = freq / N # 相対度数
+  ) |> 
+  tidyr::complete(
+    tibble::tibble(frame_i = 1:frame_num, N = 1:frame_num), x = 0:x_max, 
+    fill = list(freq = 0, rel_freq = 0)
+  ) # 未観測値を補完
+
+# ラベル用の文字列を作成
+anim_label_df <- anim_freq_df |> 
+  tidyr::pivot_wider(
+    id_cols      = c(frame_i, N), 
+    names_from   = x, 
+    names_prefix = "x", 
+    values_from  = freq
+  ) |> # 度数列を展開
+  tidyr::unite(
+    col = "freq_txt", dplyr::starts_with("x"), sep = ", "
+  ) |> # 度数情報を結合
+  dplyr::mutate(
+    param_lbl = paste0(
+      "list(", 
+      "N == ", N, ", ", 
+      "M == ", M, ", ", 
+      "phi == ", phi, 
+      ")"
+    ), # パラメータラベル
+    freq_lbl = paste0("N == (list(", freq_txt, "))") # 度数ラベル
+  ) |> 
+  dplyr::select(-freq_txt)
 
 
-# 二項乱数のヒストグラムのアニメーションを作図:度数
-anime_hist_graph <- ggplot() + 
-  geom_bar(data = anime_freq_df, mapping = aes(x = x, y = frequency), 
-           stat = "identity", fill = "#00A968") + # 度数
-  geom_point(data = anime_data_df, mapping = aes(x = x, y = 0), 
-             color = "orange", size = 5) + # サンプル
-  gganimate::transition_manual(parameter) + # フレーム
-  scale_x_continuous(breaks = x_vals, minor_breaks = FALSE) + # x軸目盛
-  labs(title = "Binomial Distribution", 
-       subtitle = "{current_frame}", 
-       x = "x", y = "frequency") # ラベル
+# サンプルの度数を作図
+anim <- ggplot() + 
+  geom_bar(
+    data    = anim_freq_df, 
+    mapping = aes(x = x, y = freq), 
+    stat = "identity", 
+    fill = "#00A968"
+  ) + # 度数
+  geom_point(
+    data    = anim_sample_df, 
+    mapping = aes(x = x, y = -Inf), 
+    color = "orange", size = 4
+  ) + # サンプル
+  geom_text(
+    data    = anim_label_df, 
+    mapping = aes(x = -Inf, y = Inf, label = param_lbl), 
+    parse = TRUE, hjust = 0, vjust = -0.5
+  ) + # パラメータのラベル
+  geom_label(
+    data    = anim_label_df, 
+    mapping = aes(x = -Inf, y = Inf, label = freq_lbl), 
+    parse = TRUE, hjust = 0, vjust = 1, alpha = 0.5
+  ) + # 度数のラベル
+  gganimate::transition_manual(frames = frame_i) + # フレーム制御
+  scale_x_continuous(breaks = x_vec, minor_breaks = FALSE) + # x軸目盛
+  theme(
+    plot.subtitle = element_text(size = 40) # (パラメータラベル用の空行サイズ)
+  ) + # 図の体裁
+  coord_cartesian(
+    xlim = c(0, x_max), 
+    clip = "off" # (パラメータラベル用の枠外描画設定)
+  ) + # 描画範囲
+  labs(
+    title = "Binomial distribution", 
+    subtitle = "", # (パラメータラベル用の空行)
+    x = expression(x), 
+    y = "frequency"
+  )
 
-# gif画像を作成
-gganimate::animate(anime_hist_graph, nframes = N, fps = 10, width = 800, height = 600)
+# 動画を作成
+gganimate::animate(
+  plot = anim, 
+  nframes = frame_num, fps = 10, 
+  width = 12, height = 8, units = "in", res = 100, 
+  renderer = gganimate::av_renderer(file = "figure/binomial/random_number/freq_1smp.mp4")
+)
 
 
-# 二項乱数のヒストグラムのアニメーションを作図:相対度数
-anime_hist_graph <- ggplot() + 
-  geom_bar(data = anime_freq_df, mapping = aes(x = x, y = frequency/n), 
-           stat = "identity", fill = "#00A968") + # 相対度数
-  geom_bar(data = anime_prob_df, mapping = aes(x = x, y = probability), 
-           stat = "identity", alpha = 0, color = "darkgreen", linetype = "dashed") + # 元の分布
-  geom_point(data = anime_data_df, mapping = aes(x = x, y = 0), 
-             color = "orange", size = 5) + # サンプル
-  gganimate::transition_manual(parameter) + # フレーム
-  scale_x_continuous(breaks = x_vals, minor_breaks = FALSE) + # x軸目盛
-  coord_cartesian(ylim = c(-0.01, 0.5)) + # 軸の表示範囲
-  labs(title = "Binomial Distribution", 
-       subtitle = "{current_frame}", 
-       x = "x", y = "relative frequency") # ラベル
+# 確率軸の範囲を指定
+prob_max <- 0.25
 
-# gif画像を作成
-gganimate::animate(anime_hist_graph, nframes = N, fps = 10, width = 800, height = 600)
+# サンプルの相対度数を作図
+anim <- ggplot() + 
+  geom_bar(
+    data    = anim_freq_df, 
+    mapping = aes(x = x, y = rel_freq, linetype = "sample"), 
+    stat = "identity", 
+    fill = "#00A968", alpha = 0.5
+  ) + # 相対度数
+  geom_bar(
+    data = prob_df, 
+    mapping = aes(x = x, y = prob, linetype = "generator"), 
+    stat = "identity", 
+    fill = NA, color = "darkgreen"
+  ) + # 生成分布
+  geom_point(
+    data    = anim_sample_df, 
+    mapping = aes(x = x, y = -Inf), 
+    color = "orange", size = 4
+  ) + # サンプル
+  geom_text(
+    data    = anim_label_df, 
+    mapping = aes(x = -Inf, y = Inf, label = param_lbl), 
+    parse = TRUE, hjust = 0, vjust = -0.5
+  ) + # パラメータのラベル
+  geom_label(
+    data    = anim_label_df, 
+    mapping = aes(x = -Inf, y = Inf, label = freq_lbl), 
+    parse = TRUE, hjust = 0, vjust = 1, alpha = 0.5
+  ) + # 度数のラベル
+  gganimate::transition_manual(frames = frame_i) + # フレーム制御
+  scale_x_continuous(breaks = x_vec, minor_breaks = FALSE) + # x軸目盛
+  scale_linetype_manual(
+    breaks = c("generator", "sample"), 
+    values = c("dashed", "solid"), 
+    labels = c("generator", "random number"), 
+    name   = "distribution"
+  ) + # 凡例の表示用
+  theme(
+    plot.subtitle = element_text(size = 40) # (パラメータラベル用の空行サイズ)
+  ) + # 図の体裁
+  coord_cartesian(
+    xlim = c(0, x_max), 
+    ylim = c(0, prob_max), 
+    clip = "off" # (パラメータラベル用の枠外描画設定)
+  ) + # 描画範囲
+  labs(
+    title = "Binomial distribution", 
+    subtitle = "", # (パラメータラベル用の空行)
+    x = expression(x), 
+    y = "relative frequency, probability"
+  )
+
+# 動画を作成
+gganimate::animate(
+  plot = anim, 
+  nframes = frame_num, fps = 10, 
+  width = 12, height = 8, units = "in", res = 100, 
+  renderer = gganimate::av_renderer(file = "figure/binomial/random_number/relfreq_1smp.mp4")
+)
+
+
+#### 複数サンプルずつ集計 -----
+
+# フレーム数を指定
+frame_num <- 300
+
+# 1フレーム当たりのサンプル数を設定
+smp_per_frame <- N %/% frame_num
+
+
+# サンプルを集計
+anim_freq_df <- tibble::tibble(
+  frame_i = 1:frame_num, # フレーム番号
+  N       = smp_per_frame*frame_i # サンプル数
+) |> 
+  dplyr::reframe(
+    n = 1:N, # サンプル番号
+    .by = c(frame_i, N)
+  ) |> 
+  dplyr::mutate(
+    x = x_n[n] # 観測値
+  ) |> 
+  dplyr::count(
+    frame_i, N, x, name = "freq" # 度数
+  ) |> 
+  dplyr::mutate(
+    rel_freq = freq / N # 相対度数
+  ) |> 
+  tidyr::complete(
+    tibble::tibble(
+      frame_i = 1:frame_num, 
+      N = smp_per_frame*(1:frame_num)
+    ), 
+    x = 0:x_max, 
+    fill = list(freq = 0, rel_freq = 0)
+  ) # 未観測値を補完
+
+# ラベル用の文字列を作成
+anim_label_df <- anim_freq_df |> 
+  tidyr::pivot_wider(
+    id_cols      = c(frame_i, N), 
+    names_from   = x, 
+    names_prefix = "x", 
+    values_from  = freq
+  ) |> # 度数列を展開
+  tidyr::unite(
+    col = "freq_txt", dplyr::starts_with("x"), sep = ", "
+  ) |> # 度数情報を結合
+  dplyr::mutate(
+    param_lbl = paste0(
+      "list(", 
+      "N == ", N, ", ", 
+      "M == ", M, ", ", 
+      "phi == ", phi, 
+      ")"
+    ), # パラメータラベル
+    freq_lbl = paste0("N == (list(", freq_txt, "))") # 度数ラベル
+  ) |> 
+  dplyr::select(-freq_txt)
+
+
+# サンプルの度数を作図
+anim <- ggplot() + 
+  geom_bar(
+    data    = anim_freq_df, 
+    mapping = aes(x = x, y = freq), 
+    stat = "identity", 
+    fill = "#00A968"
+  ) + # 度数
+  geom_text(
+    data    = anim_label_df, 
+    mapping = aes(x = -Inf, y = Inf, label = param_lbl), 
+    parse = TRUE, hjust = 0, vjust = -0.5
+  ) + # パラメータのラベル
+  geom_label(
+    data    = anim_label_df, 
+    mapping = aes(x = -Inf, y = Inf, label = freq_lbl), 
+    parse = TRUE, hjust = 0, vjust = 1, alpha = 0.5
+  ) + # 度数のラベル
+  gganimate::transition_manual(frames = frame_i) + # フレーム制御
+  scale_x_continuous(breaks = x_vec, minor_breaks = FALSE) + # x軸目盛
+  theme(
+    plot.subtitle = element_text(size = 40) # (パラメータラベル用の空行サイズ)
+  ) + # 図の体裁
+  coord_cartesian(
+    xlim = c(0, x_max), 
+    clip = "off" # (パラメータラベル用の枠外描画設定)
+  ) + # 描画範囲
+  gganimate::view_follow(fixed_y = FALSE) + # 描画領域
+  labs(
+    title = "Binomial distribution", 
+    subtitle = "", # (パラメータラベル用の空行)
+    x = expression(x), 
+    y = "frequency"
+  )
+
+# 動画を作成
+gganimate::animate(
+  plot = anim, 
+  nframes = frame_num, fps = 10, 
+  width = 12, height = 8, units = "in", res = 100, 
+  renderer = gganimate::av_renderer(file = "figure/binomial/random_number/freq_nsmp.mp4")
+)
+
+
+# サンプルの相対度数を作図
+anim <- ggplot() + 
+  geom_bar(
+    data    = anim_freq_df, 
+    mapping = aes(x = x, y = rel_freq, linetype = "sample"), 
+    stat = "identity", position = "identity", 
+    fill = "#00A968", alpha = 0.5
+  ) + # 相対度数
+  geom_bar(
+    data = prob_df, 
+    mapping = aes(x = x, y = prob, linetype = "generator"), 
+    stat = "identity", position = "identity", 
+    fill = NA, color = "darkgreen"
+  ) + # 生成分布
+  geom_line(
+    data = prob_df, 
+    mapping = aes(x = x, y = prob), 
+    color = "darkgreen", linewidth = 1, linetype = "dashed"
+  ) + # 生成分布
+  geom_point(
+    data = prob_df, 
+    mapping = aes(x = x, y = prob), 
+    color = "darkgreen", size = 3
+  ) + # 生成分布
+  geom_text(
+    data    = anim_label_df, 
+    mapping = aes(x = -Inf, y = Inf, label = param_lbl), 
+    parse = TRUE, hjust = 0, vjust = -0.5
+  ) + # パラメータのラベル
+  geom_label(
+    data    = anim_label_df, 
+    mapping = aes(x = -Inf, y = Inf, label = freq_lbl), 
+    parse = TRUE, hjust = 0, vjust = 1, alpha = 0.5
+  ) + # 度数のラベル
+  gganimate::transition_manual(frames = frame_i) + # フレーム制御
+  scale_x_continuous(breaks = x_vec, minor_breaks = FALSE) + # x軸目盛
+  scale_linetype_manual(
+    breaks = c("generator", "sample"), 
+    values = c("dashed", "solid"), 
+    labels = c("generator", "random number"), 
+    name   = "distribution"
+  ) + # 凡例の表示用
+  theme(
+    plot.subtitle = element_text(size = 40) # (パラメータラベル用の空行サイズ)
+  ) + # 図の体裁
+  coord_cartesian(
+    xlim = c(0, x_max), 
+    clip = "off" # (パラメータラベル用の枠外描画設定)
+  ) + # 描画範囲
+  labs(
+    title = "Binomial distribution", 
+    subtitle = "", # (パラメータラベル用の空行)
+    x = expression(x), 
+    y = "relative frequency, probability"
+  )
+
+# 動画を作成
+gganimate::animate(
+  plot = anim, 
+  nframes = frame_num, fps = 10, 
+  width = 12, height = 8, units = "in", res = 100, 
+  renderer = gganimate::av_renderer(file = "figure/binomial/random_number/relfreq_nsmp.mp4")
+)
 
 
