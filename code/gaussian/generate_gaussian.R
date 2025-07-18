6417d6a7dc1670743025e44c0b62498987be5e2c
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2025-07-18 20:03:48 +0900

    アニメーション化、patchworkからcowplotに変更

--- code/gaussian/generate_gaussian.R
+++ code/gaussian/generate_gaussian.R
@@ -8,115 +8,272 @@
 
 # 利用パッケージ
 library(tidyverse)
-library(patchwork)
+library(cowplot)
+library(magick)
 
 # パッケージ名の省略用
 library(ggplot2)
-library(patchwork)
 
 
+# ハイパラとサンプル分布の形状の関係 -------------------------------------------
 
-# 分布の生成 -------------------------------------------------------------------
+### パラメータの設定 -----
 
-### ・パラメータの生成 -----
+# フレーム数を指定
+frame_num <- 100
 
-# パラメータを指定
-mu_gen    <- 5
-sigma_gen <- 2.5
+# 生成分布のパラメータを指定
+mu_vals    <- seq(from = 0, to = 0, length.out = frame_num)
+sigma_vals <- seq(from = 0, to = 10, length.out = frame_num+1)[-1]
 
-# 分布の数(サンプルサイズ)を指定
-N <- 10
+# サンプル分布のパラメータを指定
+s <- 1
 
-
-# ガウス分布の平均パラメータを生成
-mu_n <- rnorm(n = N, mean = mu_gen, sd = sigma_gen) |> # ガウス分布の乱数を生成
-  sort() # 昇順に並べ替え
-
-# パラメータを格納
-param_df <- tibble::tibble(
-  mu = mu_n, 
-  parameter = round(mu_n, 3) |> 
-    factor() # 色分け用ラベル
-)
+# サンプルサイズを指定
+N <- 11
 
 
-# muの値を作成
-mu_vals <- seq(from = mu_gen - sigma_gen*3, to = mu_gen + sigma_gen*3, length.out = 251)
+# m軸の範囲を設定
+sigma_num <- 2
+u <- 5
+m_min <- (mu_vals - sigma_num*sigma_vals) |> 
+  min() |> 
+  (\(.) {floor(. /u)*u})() # u単位で切り下げ
+m_max <- (mu_vals + sigma_num*sigma_vals) |> 
+  max() |> 
+  (\(.) {ceiling(. /u)*u})() # u単位で切り上げ
 
-# 生成分布を計算
-gaussian_gen_df <- tibble::tibble(
-  mu = mu_vals, # 確率変数
-  density = dnorm(x = mu_vals, mean = mu_gen, sd = sigma_gen) # 確率密度
-)
+# m軸の値を作成
+m_vec <- seq(from = m_min, to = m_max, length.out = 1001)
 
-# 平均パラメータの期待値を計算
-E_mu <- mu_gen
+# x軸の範囲を設定
+x_min <- m_min
+x_max <- m_max
 
-
-# 生成分布とパラメータのサンプルを作図
-gaussian_gen_graph <- ggplot() + # データ
-  geom_vline(xintercept = E_mu, 
-             color = "red", size = 1, linetype = "dashed") + # パラメータの期待値
-  geom_line(data = gaussian_gen_df, mapping = aes(x = mu, y = density), 
-            color = "#00A968", size = 1) + # パラメータの生成分布
-  geom_point(data = param_df, mapping = aes(x = mu, y = 0, color = parameter), 
-             alpha = 0.8, size = 6, show.legend = FALSE) + # パラメータのサンプル
-  guides(color = guide_legend(override.aes = list(size = 2, alpha = 1))) + # 凡例の体裁
-  labs(
-    title = "Gaussian Distribution", 
-    subtitle = parse(
-      text = paste0("list(mu[paste(g,e,n)]==", mu_gen, ", sigma[paste(g,e,n)]==", sigma_gen, ")"), 
-    ), 
-    color = expression(mu), 
-    x = expression(mu), y = "density"
-  ) # ラベル
-gaussian_gen_graph
+# x軸の値を作成
+x_vec <- m_vec
 
 
-### ・分布の作図：1次元ガウス分布 -----
+### サンプル(平均パラメータ)の作図 -----
 
-# 標準偏差パラメータを指定
-sigma <- 1
+# 一時書き出し先を指定
+dir_path <- "figure/tmp_folder"
 
 
-# xの値を作成
-x_vals <- mu_vals
+# 確率密度軸の範囲を設定
+u <- 0.05
+generator_dens_max <- dnorm(x = 0, mean = 0, sd = min(sigma_vals)) |> 
+  (\(.) {ceiling(. /u)*u})() # u単位で切り上げ
+generator_dens_max <- 0.5
+sample_dens_max <- dnorm(x = 0, mean = 0, sd = s) |> 
+  (\(.) {ceiling(. /u)*u})() # u単位で切り上げ
 
-# パラメータのサンプルごとにガウス分布を計算
-res_gaussian_df <- tidyr::expand_grid(
-  x = x_vals, 
-  mu = mu_n
-) |> # 全ての組み合わせを作成
-  dplyr::arrange(mu, x) |> # パラメータごとに並べ替え
-  dplyr::mutate(
-    density = dnorm(x = x, mean = mu, sd = sigma), # 確率密度
-    parameter = round(mu, 3) |> 
-      factor() # 色分け用ラベル
+# ハイパラごとに作図
+for(i in 1:frame_num) {
+  
+  #### パラメータの生成 -----
+  
+  # パラメータを取得
+  mu    <- mu_vals[i]
+  sigma <- sigma_vals[i]
+  
+  # サンプルの期待値を計算
+  E_m <- mu
+  
+  # パラメータを生成
+  pctl_min <- pnorm(q = m_min, mean = mu, sd = sigma)             # 描画範囲の最小値を取得
+  pctl_max <- pnorm(q = m_max, mean = mu, sd = sigma)             # 描画範囲の最大値を取得
+  pctl_n   <- seq(from = pctl_min, to = pctl_max, length.out = N) # 等間隔の累積確率に設定
+  m_n      <- qnorm(p = pctl_n, mean = mu, sd = sigma)            # 分布の形状に応じて設定
+  
+  # 作図用に値を調整
+  m_n[m_n < m_min] <- m_min
+  m_n[m_n > m_max] <- m_max
+  
+  # パラメータを格納
+  param_df <- tibble::tibble(
+    n = 1:N, # サンプル番号
+    m = m_n, # 確率変数(平均パラメータ)
+    dens_max = dnorm(x = m, mean = m, sd = s) # 最頻値における確率密度
   )
+  
+  #### 生成分布の作図 -----
+  
+  # ガウス分布を計算
+  generator_df <- tidyr::tibble(
+    m    = m_vec, # 確率変数
+    dens = dnorm(x = m, mean = mu, sd = sigma) # 確率密度
+  )
+  
+  # ラベル用の文字列を作成
+  generator_param_lbl <- paste0(
+    "list(", 
+    "mu == ", round(mu, digits = 2), ", ", 
+    "sigma == ", round(sigma, digits = 2), 
+    ")"
+  ) |> 
+    parse(text = _)
+  
+  # ガウス分布を作図
+  generator_graph <- ggplot() + 
+    geom_vline(
+      xintercept = E_m, 
+      color = "red", linewidth = 1, linetype = "dashed"
+    ) + # 期待値の位置
+    geom_vline(
+      data    = param_df, 
+      mapping = aes(xintercept = m, color = factor(n)), 
+      linewidth = 1, linetype = "dotted", show.legend = FALSE
+    ) + # サンプルの位置
+    geom_line(
+      data    = generator_df, 
+      mapping = aes(x = m, y = dens), 
+      linewidth = 1
+    ) + # 生成分布
+    geom_point(
+      data    = param_df, 
+      mapping = aes(x = m, y = 0, color = factor(n)), 
+      size = 3
+    ) + # サンプル
+    theme(
+      axis.title    = element_text(size = 12), 
+      plot.title    = element_text(size = 14), 
+      plot.subtitle = element_text(size = 12)
+    ) + # 図の体裁
+    guides(color = "none") + # 凡例の体裁
+    coord_cartesian(
+      xlim = c(m_min, m_max), 
+      ylim = c(0, generator_dens_max)
+    ) + # 描画範囲
+    labs(
+      title = "Gaussian distribution", 
+      subtitle = generator_param_lbl, 
+      x = expression(m), 
+      y = expression(p(m ~"|"~ mu, sigma))
+    )
+  
+  #### サンプル分布の作図 -----
+  
+  # サンプルの期待値によるガウス分布を計算
+  E_sample_df <- tidyr::tibble(
+    x    = x_vec, # 確率変数
+    dens = dnorm(x = x, mean = E_m, sd = s) # 確率密度
+  )
+  
+  # サンプルごとにガウス分布を計算
+  res_sample_df <- tidyr::expand_grid(
+    n = 1:N,   # サンプル番号
+    x = x_vec, # 確率変数
+  ) |> # サンプルごとに変数を複製
+    dplyr::mutate(
+      m    = m_n[n], # 平均パラメータ
+      dens = dnorm(x = x, mean = m, sd = s) # 確率密度
+    )
+  
+  # ラベル用の文字列を作成
+  sample_param_lbl <- paste0(
+    "list(", 
+    "E(m) == ", round(E_m, digits = 2), ", ", 
+    "s == ", s, 
+    ")"
+  ) |> 
+    parse(text = _)
+  
+  # サンプルごとにガウス分布を作図
+  sample_graph <- ggplot() + 
+    geom_vline(
+      xintercept = E_m, 
+      color = "red", linewidth = 1, linetype ="dashed"
+    ) + # 期待値との対応
+    geom_vline(
+      data    = param_df, 
+      mapping = aes(xintercept = m, color = factor(n)), 
+      linewidth = 1, linetype = "dotted", show.legend = FALSE
+    ) + # サンプルとの対応
+    geom_line(
+      data    = E_sample_df, 
+      mapping = aes(x = x, y = dens), 
+      color = "red", linewidth = 1, linetype = "dotdash"
+    ) + # 期待値による分布
+    geom_line(
+      data    = res_sample_df, 
+      mapping = aes(x = x, y = dens, color = factor(n)), 
+      linewidth = 1
+    ) + # サンプルによる分布
+    theme(
+      axis.title    = element_text(size = 12), 
+      plot.title    = element_text(size = 14), 
+      plot.subtitle = element_text(size = 12)
+    ) + # 図の体裁
+    guides(color = "none") + # 凡例の体裁
+    coord_cartesian(
+      xlim = c(x_min, x_max), 
+      ylim = c(0, sample_dens_max)
+    ) + # 描画範囲
+    labs(
+      title = "Gaussian distribution", 
+      subtitle = sample_param_lbl, 
+      x = expression(x), 
+      y = expression(p(x ~"|"~ m, s))
+    )
+  
+  #### 凡例の作図 -----
+  
+  # ラベル用の文字列を作成
+  sample_lbl <- paste0("m == ", round(m_n, digits = 2)) |> 
+    parse(text = _)
+  
+  # 凡例用の図を作成
+  dummy_graph <- ggplot() + 
+    geom_point(
+      data    = param_df, 
+      mapping = aes(x = mu, y = dens_max, color = factor(n)), 
+      size = 3
+    ) + # サンプル
+    geom_line(
+      data = res_sample_df, 
+      mapping = aes(x = x, y = dens, color = factor(n)), 
+      linewidth = 1
+    ) + # サンプルによる分布
+    scale_color_hue(labels = sample_lbl, name = "parameter") + # パラメータのラベル
+    labs(title ="dummy")
+  
+  #### グラフの出力 -----
+  
+  # 凡例を取得
+  legend <- cowplot::get_legend(dummy_graph)
+  
+  # 凡例用の図を作成
+  legend_graph <- cowplot::ggdraw(legend) + 
+    theme(plot.background = element_rect(fill = "white", color = NA)) # (透過背景の対策用)
+  
+  # グラフを並べて描画
+  tmp_graph <- cowplot::plot_grid(
+    generator_graph, sample_graph, 
+    nrow = 2, ncol = 1, 
+    align = "hv" # (グラフ位置のズレ対策用)
+  )
+  comb_graph <- cowplot::plot_grid(
+    tmp_graph, legend_graph, 
+    nrow = 1, ncol = 2, 
+    rel_widths = c(1, 0.1) # (凡例の位置の調整用)
+  )
+  
+  # 画像ファイルを書出
+  file_path <- paste0(dir_path, "/", stringr::str_pad(i, width = nchar(frame_num), pad = "0"), ".png")
+  ggplot2::ggsave(
+    filename = file_path, plot = comb_graph, 
+    width = 12, height = 9, units = "in", dpi = 100
+  )
+  
+  # 途中経過を表示
+  message("\r", i, " / ", frame_num, appendLF = FALSE)
+}
 
-# パラメータの期待値によるガウス分布を計算
-E_gaussian_df <- tidyr::tibble(
-  x = x_vals, # 確率変数
-  density = dnorm(x = x_vals, mean = E_mu, sd = sigma) # 確率密度
-)
-
-
-# サンプルと期待値によるガウス分布を作図
-gaussian_graph <- ggplot() + 
-  geom_line(data = E_gaussian_df, mapping = aes(x = x, y = density), 
-            color = "red", size = 1, linetype = "dashed") + # 期待値による分布
-  geom_line(data = res_gaussian_df, mapping = aes(x = x, y = density, color = parameter), 
-            alpha = 0.8, size = 1) + # サンプルによる分布
-  labs(
-    title = "Gaussian Distribution", 
-    subtitle = parse(text = paste0("list(E(mu)==", E_mu, ", sigma==", sigma, ")"), ), 
-    color = expression(mu), 
-    x = "x", y = "density"
-  ) # ラベル
-gaussian_graph
-
-# グラフを並べて描画
-gaussian_gen_graph / gaussian_graph + 
-  patchwork::plot_layout(guides = "collect")
+# 動画を作成
+paste0(dir_path, "/", stringr::str_pad(1:frame_num, width = nchar(frame_num), pad = "0"), ".png") |> # ファイルパスを作成
+  magick::image_read() |> # pngファイルを読込
+  magick::image_animate(fps = 1, dispose = "previous") |> # gifファイルを作成
+  magick::image_write_video(path = "figure/gaussian/generate_gaussian/gauss.mp4", framerate = 30) -> tmp_path # mp4ファイルを書出
 
 
