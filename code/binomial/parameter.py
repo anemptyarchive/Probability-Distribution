e1f4f6aac48163caa8548378f6d6a3d2b1f23ecd
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2025-08-07 19:58:12 +0900

    図をアプデ

--- code/binomial/parameter.py
+++ code/binomial/parameter.py
@@ -1,92 +1,555 @@
 
+# 二項分布 ----------------------------------------------------------------------
+
+# パラメータの可視化
+
+
 # %%
+
+# ライブラリの読込 ---------------------------------------------------------------
 
 # ライブラリを読込
 import numpy as np
-from scipy.stats import binom
+from scipy.stats import binom, norm
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 
 # %%
 
+# パラメータの影響：成功確率 ------------------------------------------------------
+
+### パラメータの設定 -----
+
 # 試行回数を指定
 M = 10
 
-# 作図用のxの値を作成
-x_vals = np.arange(M + 1)
-
-# 作図用のphiの値を作成
+# フレームごとのパラメータを指定
 phi_vals = np.arange(start=0.0, stop=1.01, step=0.01)
 
-# 図を初期化
-fig = plt.figure(figsize=(12, 8))
-
-# 作図処理を関数として定義
-def update(i):
-    # 前フレームのグラフを初期化
-    plt.cla()
-    
-    # i回目の値を取得
-    phi = phi_vals[i]
-    
-    # 分布を計算
-    probability = binom.pmf(k=x_vals, n=M, p=phi)
-    
-    # 二項分布を作図
-    plt.bar(x=x_vals, height=probability, color='#00A968') # 棒グラフ
-    plt.xlabel('x') # x軸ラベル
-    plt.ylabel('probability') # y軸ラベル
-    plt.suptitle('Binomial Distribution', fontsize=20) # 図タイトル
-    plt.title('$\phi=' + str(np.round(phi, 2)) + ', M=' + str(M) + '$', loc='left') # タイトル
-    plt.xticks(ticks=x_vals) # x軸目盛
-    plt.grid() # グリッド線
-    plt.ylim(-0.1, 1.1) # y軸の表示範囲
-
-# gif画像を作成
-anime_prob = FuncAnimation(fig, update, frames=len(phi_vals), interval=100)
-
-# gif画像を保存
-anime_prob.save('../figure/binomial/parameter/Binomial_prob_phi.gif')
+# フレーム数を設定
+frame_num = len(phi_vals)
 
 
 # %%
 
-# パラメータを指定
-phi = 0.3
+### 変数の設定 -----
+
+# x軸の範囲を設定
+x_min = 0.0
+x_max = M
+
+# x軸の値を作成
+x_vec = np.arange(start=x_min, stop=x_max+1, step=1)
+
+
+# %%
+
+### 分布の計算 -----
+
+# 二項分布の確率を計算
+prob_lt = [
+    binom.pmf(k=x_vec, n=M, p=phi_vals[i]) for i in range(frame_num)
+]
+
+
+# %%
+
+### パラメータと形状の関係 -----
+
+# 確率軸の範囲を設定
+u = 0.05
+prob_max = np.max(prob_lt)
+prob_max = np.ceil(prob_max /u)*u # u単位で切り上げ
+
+# 図を初期化
+fig, ax = plt.subplots(figsize=(8, 6), dpi=100, facecolor='white')
+fig.suptitle('Binomial distribution', fontsize=20)
+
+# 作図処理を定義
+def update(i):
+
+    # 前フレームのグラフを初期化
+    ax.cla()
+    
+    # 値を取得
+    phi = phi_vals[i]     # パラメータ
+    prob_vec = prob_lt[i] # 確率
+    
+    # 二項分布を描画
+    ax.bar(
+        x=x_vec, height=prob_vec, 
+        color='#00A968'
+    ) # 確率
+    ax.set_xticks(ticks=x_vec) # x軸目盛
+    ax.grid()
+    ax.set_xlabel('x')
+    ax.set_ylabel('probability')
+    ax.set_title(f'$M = {M}, \\phi = {phi:.2f}$', loc='left')
+    ax.set_ylim(ymin=0.0, ymax=prob_max) # 描画範囲を固定
+
+# 動画を作成
+anime_freq = FuncAnimation(fig=fig, func=update, frames=frame_num, interval=100)
+
+# 動画を書出
+anime_freq.save(
+    filename='../figure/binomial/parameter/parameter_phi.mp4', 
+    progress_callback=lambda i, n: print(f'frame: {i} / {n}')
+)
+
+
+# %%
+
+### パラメータと統計量の関係 -----
+
+# 確率軸の範囲を設定
+u = 0.05
+prob_max = np.max(prob_lt)
+prob_max = np.ceil(prob_max /u)*u # u単位で切り上げ
+
+# 余白を追加
+y_margin = 0.05
+y_min = -prob_max * y_margin
+y_max = prob_max * (1.0+y_margin)
+
+# 図を初期化
+fig, ax = plt.subplots(figsize=(8, 6), dpi=100, facecolor='white')
+fig.suptitle('Binomial distribution', fontsize=20)
+
+# 作図処理を定義
+def update(i):
+
+    # 前フレームのグラフを初期化
+    ax.cla()
+
+    # 値を取得
+    phi = phi_vals[i]     # パラメータ
+    prob_vec = prob_lt[i] # 確率
+
+    # 統計量を計算
+    mean_x = M * phi                        # 期待値
+    sd_x   = np.sqrt(M * phi * (1.0 - phi)) # 標準偏差
+    mode_x = np.floor((M + 1) * phi)        # 最頻値
+    
+    # 二項分布を描画
+    ax.bar(
+        x=x_vec, height=prob_vec, 
+        color='#00A968'
+    ) # 確率
+    ax.vlines(
+        x=mean_x, ymin=y_min, ymax=y_max, 
+        color='black', linewidth=1.0, linestyles='--', 
+        label=f'$E[x] = \\lambda = {mean_x:.2f}$'
+    ) # 期待値の位置
+    ax.vlines(
+        x=[mean_x-sd_x, mean_x+sd_x], ymin=y_min, ymax=y_max, 
+        color='black', linewidth=1.0, linestyles=':', 
+        label=f'$\\sqrt{{V[x]}} = \\sqrt{{\\lambda}} = {sd_x:.2f}$'
+    ) # 標準偏差の位置
+    ax.hlines(
+        y=0.0, xmin=mean_x-sd_x, xmax=mean_x+sd_x, 
+        color='black', linewidth=1.0
+    ) # 標準偏差の範囲
+    ax.vlines(
+        x=mode_x, ymin=y_min, ymax=y_max, 
+        color='black', linewidth=1.0, linestyles='-.', 
+        label=f'$mode[x] = \\lfloor \\lambda \\rfloor = {mode_x:.2f}$'
+    ) # 最頻値の位置
+    ax.text(
+        x=mean_x, y=0.0, 
+        s='$E[x] \pm \\sqrt{V[x]}$', ha='center', va='top', 
+        size=8
+    ) # 統計量のラベル
+    ax.set_xticks(ticks=x_vec) # x軸目盛
+    ax.grid()
+    ax.set_xlabel('x')
+    ax.set_ylabel('probability')
+    ax.set_title(f'$M = {M}, \\phi = {phi:.2f}$', loc='left')
+    ax.legend(title='statistics', prop={'size': 8}, loc='upper left')
+    ax.set_xlim(xmin=x_min-0.5, xmax=x_max+0.5) # (垂線がはみ出す回避用)
+    ax.set_ylim(ymin=y_min, ymax=y_max) # (垂線との対応用)
+
+# 動画を作成
+anime_freq = FuncAnimation(fig=fig, func=update, frames=frame_num, interval=100)
+
+# 動画を書出
+anime_freq.save(
+    filename='../figure/binomial/parameter/stats_phi.mp4', 
+    progress_callback=lambda i, n: print(f'frame: {i} / {n}')
+)
+
+
+# %%
+
+### パラメータとモーメントの関係 -----
+
+# 確率軸の範囲を設定
+u = 0.05
+prob_max = np.max(prob_lt)
+prob_max = np.ceil(prob_max /u)*u # u単位で切り上げ
+
+# 余白を追加
+y_margin = 0.05
+y_min = -prob_max * y_margin
+y_max = prob_max * (1.0+y_margin)
+
+# 図を初期化
+fig, ax = plt.subplots(figsize=(8, 6), dpi=100, facecolor='white')
+fig.suptitle('Binomial distribution', fontsize=20)
+
+# 作図処理を定義
+def update(i):
+
+    # 前フレームのグラフを初期化
+    ax.cla()
+
+    # 値を取得
+    phi = phi_vals[i]          # パラメータ
+    pois_prob_vec = prob_lt[i] # 確率
+
+    # 統計量を計算
+    mu    = M * phi                        # 期待値
+    sigma = np.sqrt(M * phi * (1.0 - phi)) # 標準偏差
+
+    # モーメントを計算
+    if phi > 0.0:
+        skew = (1.0 - 2.0 * phi) / np.sqrt(M * phi * (1.0 - phi))        # 歪度
+        kurt = (1.0 - 6.0 * phi * (1.0 - phi)) / (M * phi * (1.0 - phi)) # 尖度
+    else: # (0除算の回避用)
+        skew = np.inf
+        kurt = np.inf
+
+    # ガウス分布の確率密度を計算
+    norm_x_vec    = np.linspace(start=x_min, stop=x_max, num=1001)
+    norm_dens_vec = norm.pdf(x=norm_x_vec, loc=mu, scale=sigma)
+    
+    # ラベル用の文字列を作成
+    moment_str  = f'skewness: {skew:.3f} \n'
+    moment_str += f'kurtosis:    {kurt:.3f}' # (スペースによる位置調整)
+
+    # ポアソン分布を描画
+    ax.bar(
+        x=x_vec, height=pois_prob_vec, 
+        color='#00A968', alpha=0.5
+    ) # ポアソン分布の確率
+    ax.scatter(
+        x=x_vec, y=pois_prob_vec, 
+        color='#00A968', s=30
+    ) # ポアソン分布の確率
+    ax.vlines(
+        x=mu, ymin=y_min, ymax=y_max, 
+        color='black', linewidth=1.0, linestyles='dashed'
+    ) # 期待値の位置
+    ax.hlines(
+        y=0.0, xmin=mu-sigma, xmax=mu+sigma, 
+        color='black', linewidth=1.0
+    ) # 標準偏差の範囲
+    for label_x, label_str in zip([mu-sigma, mu, mu+sigma], ['$-\sigma$', '$\mu$', '$+\sigma$']):
+        ax.text(
+            x=label_x, y=0.0, 
+            s=label_str, ha='center', va='top', 
+            size=10
+        ) # 統計量のラベル
+    ax.plot(
+        x_vec, pois_prob_vec, 
+        color='#00A968', linewidth=1.0, 
+        label='poisson'
+    ) # ポアソン分布の確率
+    ax.plot(
+        norm_x_vec, norm_dens_vec, 
+        color='red', linewidth=1.0, linestyle='dashed', 
+        label='gaussian'
+    ) # ガウス分布の確率密度
+    ax.text(
+        x=x_min, y=prob_max, 
+        s=moment_str, ha='left', va='top', 
+        bbox=dict(facecolor='white', alpha=0.8, edgecolor='black', linewidth=0.5), 
+        size = 10
+    ) # モーメントのラベル
+    ax.set_xticks(ticks=x_vec) # x軸目盛
+    ax.grid()
+    ax.set_xlabel('x')
+    ax.set_ylabel('probability, density')
+    ax.set_title(f'$M = {M}, \\phi = {phi:.2f}, \mu = {mu:.2f}, \sigma = {sigma:.2f}$', loc='left')
+    ax.legend(title='distribution', prop={'size': 8}, loc='upper right')
+    ax.set_xlim(xmin=x_min-0.5, xmax=x_max+0.5) # (垂線がはみ出す回避用)
+    ax.set_ylim(ymin=y_min, ymax=y_max) # (垂線との対応用)
+
+# 動画を作成
+anime_freq = FuncAnimation(fig=fig, func=update, frames=frame_num, interval=100)
+
+# 動画を書出
+anime_freq.save(
+    filename='../figure/binomial/parameter/moment_phi.mp4', 
+    progress_callback=lambda i, n: print(f'frame: {i} / {n}')
+)
+
+
+# %%
+
+# パラメータの影響：試行回数 ------------------------------------------------------
+
+### パラメータの設定 -----
 
 # 試行回数の最大値を指定
 M_max = 100
 
+# パラメータを指定
+phi = 0.4
+
+# フレーム数を設定
+frame_num = M_max + 1
+
+
+# %%
+
+### パラメータと形状の関係 -----
+
+# 確率軸の範囲を設定
+prob_max = 1.0
+
 # 図を初期化
-fig = plt.figure(figsize=(12, 8))
+fig, ax = plt.subplots(figsize=(9, 6), dpi=100, facecolor='white')
+fig.suptitle('Binomial distribution', fontsize=20)
 
-# 作図処理を関数として定義
-def update(M):
+# 作図処理を定義
+def update(i):
+
     # 前フレームのグラフを初期化
-    plt.cla()
-    
-    # 作図用のxの値を作成
-    x_vals = np.arange(M + 1)
-    
-    # 分布を計算
-    probability = binom.pmf(k=x_vals, n=M, p=phi)
-    
-    # 二項分布を作図
-    plt.bar(x=x_vals, height=probability, color='#00A968') # 棒グラフ
-    plt.xlabel('x') # x軸ラベル
-    plt.ylabel('probability') # y軸ラベル
-    plt.suptitle('Binomial Distribution', fontsize=20) # 図タイトル
-    plt.title('$\phi=' + str(np.round(phi, 2)) + ', M=' + str(M) + '$', loc='left') # タイトル
-    #plt.xticks(ticks=x_vals) # x軸目盛
-    plt.grid() # グリッド線
-    plt.ylim(-0.1, 1.1) # y軸の表示範囲
+    ax.cla()
 
-# gif画像を作成
-anime_prob = FuncAnimation(fig, update, frames=M_max, interval=100)
+    # パラメータを設定
+    M = i
+    
+    # x軸の値を作成
+    x_vec = np.arange(start=0, stop=M+1, step=1)
+    
+    # 二項分布の確率を計算
+    prob_vec = binom.pmf(k=x_vec, n=M, p=phi)
+    
+    # 二項分布を描画
+    ax.bar(
+        x=x_vec, height=prob_vec, 
+        color='#00A968'
+    ) # 確率
+    #ax.set_xticks(ticks=x_vec) # x軸目盛
+    ax.grid()
+    ax.set_xlabel('x')
+    ax.set_ylabel('probability')
+    ax.set_title(f'$M = {M}, \\phi = {phi}$', loc='left')
+    ax.set_ylim(ymin=0.0, ymax=prob_max) # 描画範囲を固定
 
-# gif画像を保存
-anime_prob.save('../figure/binomial/parameter/Binomial_prob_M.gif')
+# 動画を作成
+anime_freq = FuncAnimation(fig=fig, func=update, frames=frame_num, interval=100)
+
+# 動画を書出
+anime_freq.save(
+    filename='../figure/binomial/parameter/parameter_M.mp4', 
+    progress_callback=lambda i, n: print(f'frame: {i} / {n}')
+)
+
+
+# %%
+
+### パラメータと統計量の関係 -----
+
+# 確率軸の範囲を設定
+prob_max = 1.0
+
+# 余白を追加
+y_margin = 0.05
+y_min = -prob_max * y_margin
+y_max = prob_max * (1.0+y_margin)
+
+# 図を初期化
+fig, ax = plt.subplots(figsize=(9, 6), dpi=100, facecolor='white')
+fig.suptitle('Binomial distribution', fontsize=20)
+
+# 作図処理を定義
+def update(i):
+
+    # 前フレームのグラフを初期化
+    ax.cla()
+
+    # パラメータを設定
+    M = i
+    
+    # x軸の値を作成
+    x_vec = np.arange(start=0, stop=M+1, step=1)
+    
+    # 二項分布の確率を計算
+    prob_vec = binom.pmf(k=x_vec, n=M, p=phi)
+
+    # 統計量を計算
+    mean_x = M * phi                        # 期待値
+    sd_x   = np.sqrt(M * phi * (1.0 - phi)) # 標準偏差
+    mode_x = np.floor((M + 1) * phi)        # 最頻値
+    
+    # 二項分布を描画
+    ax.bar(
+        x=x_vec, height=prob_vec, 
+        color='#00A968'
+    ) # 確率
+    ax.vlines(
+        x=mean_x, ymin=y_min, ymax=y_max, 
+        color='black', linewidth=1.0, linestyles='--', 
+        label=f'$E[x] = \\lambda = {mean_x:.2f}$'
+    ) # 期待値の位置
+    ax.vlines(
+        x=[mean_x-sd_x, mean_x+sd_x], ymin=y_min, ymax=y_max, 
+        color='black', linewidth=1.0, linestyles=':', 
+        label=f'$\\sqrt{{V[x]}} = \\sqrt{{\\lambda}} = {sd_x:.2f}$'
+    ) # 標準偏差の位置
+    ax.hlines(
+        y=0.0, xmin=mean_x-sd_x, xmax=mean_x+sd_x, 
+        color='black', linewidth=1.0
+    ) # 標準偏差の範囲
+    ax.vlines(
+        x=mode_x, ymin=y_min, ymax=y_max, 
+        color='black', linewidth=1.0, linestyles='-.', 
+        label=f'$mode[x] = \\lfloor \\lambda \\rfloor = {mode_x:.2f}$'
+    ) # 最頻値の位置
+    ax.text(
+        x=mean_x, y=0.0, 
+        s='$E[x] \pm \\sqrt{V[x]}$', ha='center', va='top', 
+        size=8
+    ) # 統計量のラベル
+    #ax.set_xticks(ticks=x_vec) # x軸目盛
+    ax.grid()
+    ax.set_xlabel('x')
+    ax.set_ylabel('probability')
+    ax.set_title(f'$M = {M}, \\phi = {phi}$', loc='left')
+    ax.legend(title='statistics', prop={'size': 8}, loc='upper left')
+    ax.set_xlim(xmin=-0.5, xmax=M+0.5) # (垂線がはみ出す回避用)
+    ax.set_ylim(ymin=y_min, ymax=y_max) # (垂線との対応用)
+
+# 動画を作成
+anime_freq = FuncAnimation(fig=fig, func=update, frames=frame_num, interval=100)
+
+# 動画を書出
+anime_freq.save(
+    filename='../figure/binomial/parameter/stats_M.mp4', 
+    progress_callback=lambda i, n: print(f'frame: {i} / {n}')
+)
+
+
+# %%
+
+### パラメータとモーメントの関係 -----
+
+# 確率軸の範囲を設定
+prob_max = 1.0
+
+# y軸の余白を追加
+y_margin = 0.05
+y_min = -prob_max * y_margin
+y_max = prob_max * (1.0+y_margin)
+
+# x軸の余白を指定
+x_margin = 5
+
+# 図を初期化
+fig, ax = plt.subplots(figsize=(9, 6), dpi=100, facecolor='white')
+fig.suptitle('Binomial distribution', fontsize=20)
+
+# ラベルの位置を設定
+momemt_lbl = fig.text(
+    x=0.02, y=0.98, 
+    s='', transform=ax.transAxes, ha='left', va='top', 
+    bbox=dict(facecolor='white', alpha=0.8, edgecolor='black', linewidth=0.5), 
+    size = 10
+) # モーメントのラベル
+
+# 作図処理を定義
+def update(i):
+
+    # 前フレームのグラフを初期化
+    ax.cla()
+
+    # パラメータを設定
+    M = i
+    
+    # x軸の範囲を設定
+    x_min = -x_margin
+    x_max = M + x_margin
+
+    # x軸の値を作成
+    bimom_x_vec = np.arange(start=x_min, stop=x_max+1, step=1)
+    norm_x_vec  = np.linspace(start=x_min, stop=x_max, num=1001)
+    
+    # 二項分布の確率を計算
+    binom_prob_vec = binom.pmf(k=bimom_x_vec, n=M, p=phi)
+
+    # 統計量を計算
+    mu    = M * phi                        # 期待値
+    sigma = np.sqrt(M * phi * (1.0 - phi)) # 標準偏差
+
+    # モーメントを計算
+    if M > 0:
+        skew = (1.0 - 2.0 * phi) / np.sqrt(M * phi * (1.0 - phi))        # 歪度
+        kurt = (1.0 - 6.0 * phi * (1.0 - phi)) / (M * phi * (1.0 - phi)) # 尖度
+    else: # (0除算の回避用)
+        skew = np.inf
+        kurt = np.inf
+    
+    # ガウス分布の確率密度を計算
+    norm_dens_vec = norm.pdf(x=norm_x_vec, loc=mu, scale=sigma)
+    
+    # ラベルの文字列を設定
+    moment_str  = f'skewness: {skew:.3f} \n'
+    moment_str += f'kurtosis:    {kurt:.3f}' # (スペースによる位置調整)
+    momemt_lbl.set_text(moment_str)
+
+    # 二項分布を描画
+    ax.bar(
+        x=bimom_x_vec, height=binom_prob_vec, 
+        color='#00A968', alpha=0.5
+    ) # 二項分布の確率
+    ax.scatter(
+        x=bimom_x_vec, y=binom_prob_vec, 
+        color='#00A968', s=30
+    ) # 二項分布の確率
+    ax.vlines(
+        x=mu, ymin=y_min, ymax=y_max, 
+        color='black', linewidth=1.0, linestyles='dashed'
+    ) # 期待値の位置
+    ax.hlines(
+        y=0.0, xmin=mu-sigma, xmax=mu+sigma, 
+        color='black', linewidth=1.0
+    ) # 標準偏差の範囲
+    for label_x, label_str in zip([mu-sigma, mu, mu+sigma], ['$-\sigma$', '$\mu$', '$+\sigma$']):
+        ax.text(
+            x=label_x, y=0.0, 
+            s=label_str, ha='center', va='top', 
+            size=10
+        ) # 統計量のラベル
+    ax.plot(
+        bimom_x_vec, binom_prob_vec, 
+        color='#00A968', linewidth=1.0, 
+        label='poisson'
+    ) # 二項分布の確率
+    ax.plot(
+        norm_x_vec, norm_dens_vec, 
+        color='red', linewidth=1.0, linestyle='dashed', 
+        label='gaussian'
+    ) # ガウス分布の確率密度
+    #ax.set_xticks(ticks=x_vec) # x軸目盛
+    ax.grid()
+    ax.set_xlabel('x')
+    ax.set_ylabel('probability, density')
+    ax.set_title(f'$M = {M}, \\phi = {phi}, \mu = {mu:.1f}, \sigma = {sigma:.2f}$', loc='left')
+    ax.legend(title='distribution', prop={'size': 8}, loc='upper right')
+    ax.set_xlim(xmin=x_min-0.5, xmax=x_max+0.5) # (垂線がはみ出す回避用)
+    ax.set_ylim(ymin=y_min, ymax=y_max) # (垂線との対応用)
+
+# 動画を作成
+anime_freq = FuncAnimation(fig=fig, func=update, frames=frame_num, interval=100)
+
+# 動画を書出
+anime_freq.save(
+    filename='../figure/binomial/parameter/moment_M.mp4', 
+    progress_callback=lambda i, n: print(f'frame: {i} / {n}')
+)
 
 
 # %%
--- code/binomial/rundom_number.py
+++ code/binomial/rundom_number.py
@@ -1,7 +1,14 @@
 
+# 二項分布 ----------------------------------------------------------------------
+
+# 乱数の可視化
+
+
 # %%
 
-# 利用するライブラリ
+# ライブラリの読込 ---------------------------------------------------------------
+
+# ライブラリを読込
 import numpy as np
 from scipy.stats import binom
 import matplotlib.pyplot as plt
@@ -10,97 +17,296 @@ from matplotlib.animation import FuncAnimation
 
 # %%
 
-# パラメータを指定
-phi = 0.3
+# 乱数と生成分布の関係 ---------------------------------------------------------
+
+### パラメータの設定 -----
 
 # 試行回数を指定
 M = 10
 
-# データ数を指定
+# パラメータを指定
+phi = 0.3
+
+
+# %%
+
+### 乱数の生成 -----
+
+# サンプルサイズを指定
 N = 1000
 
-# 二項分布に従う乱数を生成
+# 二項分布の乱数を生成
 x_n = np.random.binomial(n=M, p=phi, size=N)
 
 
 # %%
 
-# 作図用のxの値を作成
-x_vals = np.arange(M + 1)
+### 変数の設定 -----
 
-# 分布を計算
-probability = binom.pmf(k=x_vals, n=M, p=phi)
+# x軸の値を作成
+x_vec = np.arange(start=0, stop=M+1, step=1)
 
 
 # %%
+
+### 分布の計算 -----
+
+# 二項分布の確率を計算
+prob_vec = binom.pmf(k=x_vec, n=M, p=phi)
+
+
+# %%
+
+### 乱数の可視化 -----
+
+#### 1サンプルずつ集計 -----
 
 # フレーム数を指定
-N = 100
-
-# 図を初期化
-fig = plt.figure(figsize=(12, 8))
-
-# 頻度の最大値を取得
-y_max = np.max([np.sum(x_n[:N] == m) for m in range(M + 1)])
-
-# 作図処理を関数として定義
-def update(n):
-    # 前フレームのグラフを初期化
-    plt.cla()
-    
-    # n個の乱数を集計
-    frequency = np.array([np.sum(x_n[:(n+1)] == m) for m in range(M + 1)])
-    
-    # サンプルのヒストグラムを作成
-    plt.bar(x=x_vals, height=frequency, color='#00A968', zorder=1) # ヒストグラム
-    plt.scatter(x=x_n[n], y=0.0, color='orange', s=100, zorder=2) # サンプル
-    plt.xlabel('x') # x軸ラベル
-    plt.ylabel('frequency') # y軸ラベル
-    plt.suptitle('Binomial Distribution', fontsize=20) # 図タイトル
-    plt.title('$\phi=' + str(phi) + ', N=' + str(n) + 
-              '=(' + ', '.join([str(f) for f in frequency]) + ')$', loc='left') # タイトル
-    plt.xticks(ticks=x_vals) # x軸目盛
-    plt.grid() # グリッド線
-    plt.ylim(-1.0, y_max + 1.0) # y軸の表示範囲
-
-# gif画像を作成
-anime_freq = FuncAnimation(fig, update, frames=N, interval=100)
-
-# gif画像を保存
-anime_freq.save('../figure/binomial/random_number/Binomial_freq.gif')
+frame_num = 300
 
 
 # %%
 
+##### 度数の作図 -----
+
+# 度数軸の範囲を設定
+u = 5.0
+freq_max = np.max([np.sum(x_n[:frame_num] == x) for x in x_vec]) # 対象を抽出して集計
+freq_max = np.ceil(freq_max /u)*u # u単位で切り上げ
+
+
 # 図を初期化
-fig = plt.figure(figsize=(9, 6))
+fig, ax = plt.subplots(figsize=(8, 6), dpi=100, facecolor='white')
+fig.suptitle('Binomial distribution', fontsize=20)
 
-# 作図処理を関数として定義
+
+# 作図処理を定義
 def update(n):
+
     # 前フレームのグラフを初期化
-    plt.cla()
-    
-    # n個の乱数を集計
-    frequency = np.array([np.sum(x_n[:(n+1)] == m) for m in range(M + 1)])
-    
-    # サンプルの構成比を作成
-    plt.bar(x=x_vals, height=probability, color='white', edgecolor='green', linestyle='--', zorder=1) # 分布
-    plt.bar(x=x_vals, height=frequency / (n + 1), color='#00A968', alpha=0.8, zorder=2) # 構成比
-    plt.scatter(x=x_n[n], y=0.0, color='orange', s=100, zorder=3) # サンプル
-    plt.xlabel('x') # x軸ラベル
-    plt.ylabel('proportion') # y軸ラベル
-    plt.suptitle('Binomial Distribution', fontsize=20) # 図タイトル
-    plt.title('$\phi=' + str(phi) + ', N=' + str(n) + 
-              '=(' + ', '.join([str(f) for f in frequency]) + ')$', loc='left') # タイトル
-    plt.xticks(ticks=x_vals) # x軸目盛
-    plt.grid() # グリッド線
-    plt.ylim(-0.1, 1.1) # y軸の表示範囲
+    ax.cla()
 
-# gif画像を作成
-anime_prop = FuncAnimation(fig, update, frames=N, interval=100)
+    # 値を調整
+    n += 1
+    
+    # サンプルを集計
+    freq_vec = np.array([np.sum(x_n[:n] == x) for x in range(M+1)])
+    
+    # サンプルの度数を描画
+    ax.bar(
+        x=x_vec, height=freq_vec, 
+        color='#00A968', zorder=0
+    ) # 度数
+    ax.scatter(
+        x=x_n[n-1], y=0.0, 
+        c='orange', s=50, clip_on=False, zorder=1
+    ) # サンプル
+    ax.set_xticks(ticks=x_vec) # x軸目盛
+    ax.grid()
+    ax.set_xlabel('x')
+    ax.set_ylabel('frequency')
+    ax.set_title(f'$N = {n}, M = {M}, \\phi = {phi}$', loc='left')
+    ax.set_ylim(ymin=0.0, ymax=freq_max) # 描画範囲を固定
 
-# gif画像を保存
-anime_prop.save('../figure/binomial/random_number/Binomial_prop.gif')
+# 動画を作成
+anime_freq = FuncAnimation(fig=fig, func=update, frames=frame_num, interval=100)
+
+# 動画を書出
+anime_freq.save(
+    filename='../figure/binomial/random_number/freq_1smp.mp4', 
+    progress_callback=lambda i, n: print(f'frame: {i} / {n}')
+)
 
 
 # %%
+
+##### 相対度数の作図 -----
+
+# 相対度数軸の範囲を設定
+relfreq_max = 0.25
+
+# 図を初期化
+fig, ax = plt.subplots(figsize=(8, 6), dpi=100, facecolor='white')
+fig.suptitle('Binomial distribution', fontsize=20)
+ax2 = ax.twinx()
+
+# 作図処理を定義
+def update(n):
+
+    # 前フレームのグラフを初期化
+    ax.cla()
+    ax2.cla()
+
+    # 値を調整
+    n += 1
+    
+    # サンプルを集計
+    freq_vec = np.array([np.sum(x_n[:n] == x) for x in range(M+1)])
+    
+    # サンプルの相対度数を描画
+    ax.bar(
+        x=x_vec, height=freq_vec/n, 
+        color='#00A968', alpha=0.5, 
+        label='random number', zorder=0
+    ) # 相対度数
+    ax.bar(
+        x=x_vec, height=prob_vec, 
+        facecolor='none', edgecolor='green', linewidth=1.0, linestyle='--', 
+        label='generator', zorder=1
+    ) # 確率
+    ax.scatter(
+        x=x_n[n-1], y=0.0, 
+        c='orange', s=50, clip_on=False, zorder=2
+    ) # サンプル
+    ax.set_xticks(ticks=x_vec) # x軸目盛
+    ax.grid()
+    ax.set_xlabel('x')
+    ax.set_ylabel('relative frequency, probability')
+    ax.set_title(f'$N = {n}, M = {M}, \\phi = {phi}$', loc='left')
+    ax.legend(title='distribution')
+    ax.set_ylim(ymin=0.0, ymax=relfreq_max) # (目盛の共通化用)
+
+    # 2軸を設定
+    relfreq_vals = ax.get_yticks()
+    freq_vals    = relfreq_vals * n
+    ax2.set_yticks(ticks=freq_vals, labels=[f'{y:.1f}' for y in freq_vals]) # 度数軸目盛
+    ax2.set_ylabel('frequency')
+    ax2.yaxis.set_label_position(position='right') # (ラベルの表示位置が初期化される対策)
+    ax2.set_ylim(ymin=0.0, ymax=relfreq_max*n) # (目盛の共通化用)
+
+# 動画を作成
+anime_freq = FuncAnimation(fig=fig, func=update, frames=frame_num, interval=100)
+
+# 動画を書出
+anime_freq.save(
+    filename='../figure/binomial/random_number/relfreq_1smp.mp4', 
+    progress_callback=lambda i, n: print(f'frame: {i} / {n}')
+)
+
+
+# %%
+
+#### 複数サンプルずつ集計 -----
+
+# フレーム数を指定
+frame_num = 300
+
+# 1フレーム当たりのサンプル数を設定
+smp_per_frame = N // frame_num
+
+
+# %%
+
+##### 度数の作図 -----
+
+# 度数軸の範囲を設定
+u = 5.0
+freq_max = np.max([np.sum(x_n[:(smp_per_frame*frame_num)] == x) for x in range(M+1)]) # 対象を抽出して集計
+freq_max = np.ceil(freq_max /u)*u # u単位で切り上げ
+
+# 図を初期化
+fig, ax = plt.subplots(figsize=(8, 6), dpi=100, facecolor='white')
+fig.suptitle('Binomial distribution', fontsize=20)
+
+# 作図処理を定義
+def update(n):
+
+    # 前フレームのグラフを初期化
+    ax.cla()
+
+    # 値を調整
+    n = smp_per_frame * (n+1)
+    
+    # サンプルを集計
+    freq_vec = np.array([np.sum(x_n[:n] == x) for x in x_vec])
+
+    # サンプルの度数を描画
+    ax.bar(
+        x=x_vec, height=freq_vec, 
+        color='#00A968'
+    ) # 度数
+    ax.set_xticks(ticks=x_vec) # x軸目盛
+    ax.grid()
+    ax.set_xlabel('x')
+    ax.set_ylabel('frequency')
+    ax.set_title(f'$N = {n}, M = {M}, \\phi = {phi}$', loc='left')
+    #ax.set_ylim(ymin=0.0, ymax=freq_max) # 描画範囲を固定
+
+# 動画を作成
+anime_freq = FuncAnimation(fig=fig, func=update, frames=frame_num, interval=100)
+
+# 動画を書出
+anime_freq.save(
+    filename='../figure/binomial/random_number/freq_nsmp.mp4', 
+    progress_callback=lambda i, n: print(f'frame: {i} / {n}')
+)
+
+
+# %%
+
+##### 相対度数の作図 -----
+
+# 相対度数軸の範囲を設定
+u = 0.05
+relfreq_max = np.max(prob_vec)
+relfreq_max = np.ceil(relfreq_max /u)*u # u単位で切り上げ
+relfreq_max = 0.25
+
+# 図を初期化
+fig, ax = plt.subplots(figsize=(8, 6), dpi=100, facecolor='white')
+fig.suptitle('Binomial distribution', fontsize=20)
+ax2 = ax.twinx()
+
+# 作図処理を定義
+def update(n):
+    
+    # 前フレームのグラフを初期化
+    ax.cla()
+    ax2.cla()
+
+    # 値を調整
+    n = smp_per_frame * (n+1)
+    
+    # サンプルを集計
+    freq_vec = np.array([np.sum(x_n[:n] == x) for x in x_vec])
+
+    # サンプルの相対度数を描画
+    ax.bar(
+        x=x_vec, height=freq_vec/n, 
+        color='#00A968', alpha=0.5, 
+        label='random number'
+    ) # 相対度数
+    ax.bar(
+        x=x_vec, height=prob_vec, 
+        facecolor='none', edgecolor='green', linewidth=1.0, linestyle='--', 
+        label='generator'
+    ) # 確率
+    ax.set_xticks(ticks=x_vec) # x軸目盛
+    ax.grid()
+    ax.set_xlabel('x')
+    ax.set_ylabel('relative frequency, probability')
+    ax.set_title(f'$N = {n}, M = {M}, \\phi = {phi}$', loc='left')
+    ax.legend(title='distribution')
+    ax.set_ylim(ymin=0.0, ymax=relfreq_max) # (目盛の共通化用)
+
+    # 2軸を設定
+    relfreq_vals = ax.get_yticks()
+    freq_vals    = relfreq_vals * n
+    ax2.set_yticks(ticks=freq_vals, labels=[f'{y:.1f}' for y in freq_vals]) # 度数軸目盛
+    ax2.set_ylabel('frequency')
+    ax2.yaxis.set_label_position(position='right') # (ラベルの表示位置が初期化される対策)
+    ax2.set_ylim(ymin=0.0, ymax=relfreq_max*n) # (目盛の共通化用)
+
+# 動画を作成
+anime_freq = FuncAnimation(fig=fig, func=update, frames=frame_num, interval=100)
+
+# 動画を書出
+anime_freq.save(
+    filename='../figure/binomial/random_number/relfreq_nsmp.mp4', 
+    progress_callback=lambda i, n: print(f'frame: {i} / {n}')
+)
+
+
+ # %%
+
+
