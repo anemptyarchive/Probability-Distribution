8207df6cb3c4c9e44bf314c8bbb1763d8853ce0e
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2026-01-13 16:06:17 +0900

    全体をアプデ

--- code/bernoulli/parameter.py
+++ code/bernoulli/parameter.py
@@ -1,74 +1,350 @@
-# ベルヌーイ分布
+# ベルヌーイ分布 ----------------------------------------------------------------
 
-# 利用するライブラリ
+# パラメータの可視化
+
+
+# %%
+
+# ライブラリの読込 ---------------------------------------------------------------
+
+# ライブラリを読込
 import numpy as np
-from scipy.stats import bernoulli, binom, multinomial # ベルヌーイ分布, 二項分布, 多項分布
+from scipy.stats import bernoulli, norm
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-#%%
 
-### グラフの作成
+# %%
 
-# パラメータを指定
-phi = 0.3
+# パラメータの影響 ---------------------------------------------------------------
 
-# 作図用のxの値を作成
-x_vals = np.array([0.0, 1.0])
+### パラメータの設定 -----
 
-# 分布を計算
-probability = np.array([1.0 - phi, phi])
-
-# ベルヌーイ分布を作図
-plt.figure(figsize=(9, 8)) # 図の設定
-plt.bar(x=x_vals, height=probability, color='#00A968') # 棒グラフ
-#plt.vlines(x=E_x, ymin=0.0, ymax=np.max(probability), color='orange', linestyle='--', label='$E[x]$') # 平均
-#plt.vlines(x=E_x - V_x, ymin=0.0, ymax=np.max(probability), color='orange', linestyle=':', label='$E[x] - \sqrt{V[x]}$') # 平均 - 標準偏差
-#plt.vlines(x=E_x + V_x, ymin=0.0, ymax=np.max(probability), color='orange', linestyle=':', label='$E[x] + \sqrt{V[x]}$') # 平均 + 標準偏差
-plt.xlabel('x') # x軸ラベル
-plt.ylabel('probability') # y軸ラベル
-plt.suptitle('Bernoulli Distribution', fontsize=20) # 図タイトル
-plt.title('$\phi=' + str(phi) + '$', loc='left') # タイトル
-plt.xticks(ticks=x_vals) # x軸目盛
-#plt.legend() # 凡例
-plt.grid() # グリッド線
-plt.show() # 描画
-
-#%%
-
-### パラメータと分布の形状の関係
-
-# 作図用のphiの値を作成
+# フレームごとのパラメータを指定
 phi_vals = np.arange(start=0.0, stop=1.01, step=0.01)
 
+# フレーム数を設定
+frame_num = len(phi_vals)
+
+
+# %%
+
+### 変数の設定 -----
+
+# x軸の範囲を設定
+x_min = 0
+x_max = 1
+print('x size:', x_min, x_max)
+
+# x軸の余白を指定:(「モーメントとの関係」用)
+x_margin = 0
+x_min -= x_margin
+x_max += x_margin
+
+# x軸の値を作成
+x_vec = np.arange(start=x_min, stop=x_max+1, step=1)
+
+
+# %%
+
+### 分布の計算 -----
+
+# ベルヌーイ分布の確率を計算
+prob_lt = [
+    bernoulli.pmf(k=x_vec, p=phi_vals[i]) for i in range(frame_num)
+]
+
+
+# %%
+
+### 分布の作図 -----
+
+### パラメータと形状の関係 -----
+
+# 確率軸の範囲を設定
+u = 0.05
+prob_max = np.max(prob_lt)
+prob_max = np.ceil(prob_max /u)*u # u単位で切り上げ
+print('p(x) size:', prob_max)
+
 # 図を初期化
-fig = plt.figure(figsize=(9, 8))
+fig, ax = plt.subplots(figsize=(8, 6), dpi=100, facecolor='white')
+fig.suptitle('Bernoulli distribution', fontsize=20)
 
-# 作図処理を関数として定義
+# 初期化処理を定義
+def init():
+    pass
+
+# 作図処理を定義
 def update(i):
+
     # 前フレームのグラフを初期化
-    plt.cla()
+    ax.cla()
     
-    # i回目の値を取得
-    phi = phi_vals[i]
+    # 値を取得
+    phi = phi_vals[i] # 成功確率パラメータ
+    prob_vec = prob_lt[i] # 確率
+
+    # ラベル用の文字列を作成
+    param_lbl = f'$\\phi = {phi:.2f}$'
     
-    # ベルヌーイ分布を作図
-    plt.bar(x=[0.0, 1.0], height=[1.0 - phi, phi], color='#00A968') # 棒グラフ
-    plt.xlabel('x') # x軸ラベル
-    plt.ylabel('probability') # y軸ラベル
-    plt.suptitle('Bernoulli Distribution', fontsize=20) # 図タイトル
-    plt.title('$\phi=' + str(np.round(phi, 2)) + '$', loc='left') # タイトル
-    plt.xticks(ticks=[0, 1]) # x軸目盛
-    plt.grid() # グリッド線
-    plt.ylim(-0.1, 1.1) # y軸の表示範囲
+    # ベルヌーイ分布を描画
+    ax.bar(
+        x=x_vec, height=prob_vec, 
+        color='#00A968'
+    ) # 確率
+    ax.set_xticks(ticks=x_vec) # x軸目盛
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('probability')
+    ax.set_title(param_lbl, loc='left')
+    ax.grid()
+    ax.set_ylim(ymin=0.0, ymax=prob_max) # 描画範囲を固定
 
-# gif画像を作成
-anime_prob = FuncAnimation(fig, update, frames=len(phi_vals), interval=100)
+# 動画を作成
+anim = FuncAnimation(
+    fig=fig, func=update, init_func=init, 
+    frames=frame_num, interval=100
+)
 
-# gif画像を保存
-anime_prob.save('ProbabilityDistribution/Bernoulli_prob.gif')
+# 動画を書出
+anim.save(
+    filename='../../figure/bernoulli/parameter/parameter_phi.mp4', 
+    progress_callback=lambda i, n: print(f'\rframe: {i+1} / {n}', end='', flush=True)
+)
 
-#%%
 
-print('end')
+# %%
+
+### パラメータと統計量の関係 -----
+
+# 確率軸の範囲を設定
+u = 0.05
+prob_max = np.max(prob_lt)
+prob_max = np.ceil(prob_max /u)*u # u単位で切り上げ
+print('p(x) size:', prob_max)
+
+# ラベルの表示用の余白を設定
+y_margin = 0.05
+y_min = -prob_max * y_margin
+y_max = prob_max * (1.0+y_margin)
+
+# 図を初期化
+fig, ax = plt.subplots(figsize=(8, 6), dpi=100, facecolor='white')
+fig.suptitle('Bernoulli distribution', fontsize=20)
+
+# 初期化処理を定義
+def init():
+    pass
+
+# 作図処理を定義
+def update(i):
+
+    # 前フレームのグラフを初期化
+    ax.cla()
+
+    # 値を取得
+    phi = phi_vals[i] # 成功確率パラメータ
+    prob_vec = prob_lt[i] # 確率
+
+    # 統計量を計算
+    mean_x = phi                      # 期待値
+    sd_x   = np.sqrt(phi * (1.0-phi)) # 標準偏差
+    mode_x = 1 if phi >= 0.5 else 0   # 最頻値
+
+    # ラベル用の文字列を作成
+    param_lbl = f'$\\phi = {phi:.2f}$'
+    
+    # ベルヌーイ分布を描画
+    ax.bar(
+        x=x_vec, height=prob_vec, 
+        color='#00A968', alpha=0.5, 
+        zorder=10
+    ) # 確率
+    ax.axvline(
+        x=mean_x, 
+        color='black', linewidth=1.0, linestyle='--', 
+        label=f'$E[x] = \\phi = {mean_x:.2f}$', 
+        zorder=11
+    ) # 期待値の位置
+    for i, coord_x in enumerate([mean_x-sd_x, mean_x+sd_x]):
+        ax.axvline(
+            x=coord_x, 
+            color='black', linewidth=1.0, linestyle=':', 
+            label=f'$\\sqrt{{V[x]}} = \\sqrt{{\\phi (1-\\phi)}} = {sd_x:.2f}$' if i == 0 else None, 
+        zorder=11
+        ) # 標準偏差の位置
+    ax.hlines(
+        y=0.0, xmin=mean_x-sd_x, xmax=mean_x+sd_x, 
+        color='black', linewidth=1.0, 
+        zorder=11
+    ) # 標準偏差の範囲
+    ax.axvline(
+        x=mode_x, 
+        color='black', linewidth=1.0, linestyle='-.', 
+        label=f'$mode[x] = 0\ (\phi \leq 0.5), 1\ (\phi \geq 0.5) = {mode_x:.2f}$', 
+        zorder=11
+    ) # 最頻値の位置
+    ax.text(
+        x=mean_x, y=0.0, 
+        s='$E[x] \pm \\sqrt{V[x]}$', ha='center', va='top', 
+        size=8, 
+        zorder=12
+    ) # 統計量のラベル
+    ax.set_xticks(ticks=x_vec) # x軸目盛
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('probability')
+    ax.set_title(param_lbl, loc='left')
+    ax.legend(title='statistics', prop={'size': 8}, loc='upper left')
+    ax.grid(zorder=0)
+    ax.set_xlim(xmin=x_min-0.5, xmax=x_max+0.5) # (垂直線がはみ出すときの対策用)
+    ax.set_ylim(ymin=y_min, ymax=y_max) # (ラベルの表示用)
+
+# 動画を作成
+anim = FuncAnimation(
+    fig=fig, func=update, init_func=init, 
+    frames=frame_num, interval=100
+)
+
+# 動画を書出
+anim.save(
+    filename='../../figure/bernoulli/parameter/stats_phi.mp4', 
+    progress_callback=lambda i, n: print(f'\rframe: {i+1} / {n}', end='', flush=True)
+)
+
+
+# %%
+
+### パラメータとモーメントの関係 -----
+
+# 確率軸の範囲を設定
+u = 0.05
+prob_max = np.max(prob_lt)
+prob_max = np.ceil(prob_max /u)*u # u単位で切り上げ
+print('p(x) size:', prob_max)
+
+# 統計量ラベルの表示用の余白を設定
+y_margin = 0.05
+y_min = -prob_max * y_margin
+y_max = prob_max * (1.0+y_margin)
+
+# ラベルの表示位置を設定
+moment_loc_x = 0.02
+moment_loc_y = 0.97
+
+# 図を初期化
+fig, ax = plt.subplots(figsize=(9, 6), dpi=100, facecolor='white')
+fig.suptitle('Bernoulli distribution', fontsize=20)
+
+# 初期化処理を定義
+def init():
+    pass
+
+# 作図処理を定義
+def update(i):
+
+    # 前フレームのグラフを初期化
+    ax.cla()
+
+    # 値を取得
+    phi = phi_vals[i] # 成功確率パラメータ
+    prob_vec = prob_lt[i] # 確率
+
+    # 統計量を計算
+    mu    = phi                      # 期待値
+    sigma = np.sqrt(phi * (1.0-phi)) # 標準偏差
+
+    # モーメントを計算
+    if phi > 0.0 and phi < 1.0: # パラメータの条件
+        skew  = 1.0 - 2.0 * phi             # 歪度
+        skew /= np.sqrt(phi * (1.0-phi))
+        kurt  = 1.0 - 6.0 * phi * (1.0-phi) # 尖度
+        kurt /= phi * (1.0-phi)
+    else:
+        skew = np.inf
+        kurt = np.inf
+
+    # ガウス分布の確率密度を計算
+    norm_x_vec = np.linspace(start=x_min, stop=x_max, num=1001)
+    if sigma > 0.0: # パラメータの条件
+        norm_dens_vec = norm.pdf(x=norm_x_vec, loc=mu, scale=sigma)
+    else:
+        norm_dens_vec = np.tile(np.nan, reps=len(norm_x_vec))
+    
+    # ラベル用の文字列を作成
+    param_lbl   = f'$\\phi = {phi:.2f}, '
+    param_lbl  += f'\\mu = {mu:.2f}, \\sigma = {sigma:.2f}$'
+    moment_lbl  = f'skewness: {skew:.3f}\n'
+    moment_lbl += f'kurtosis:    {kurt:.3f}' # (スペースによる位置調整)
+    
+    # ベルヌーイ分布を描画
+    ax.bar(
+        x=x_vec, height=prob_vec, 
+        color='#00A968', alpha=0.5, 
+        zorder=10
+    ) # ベルヌーイ分布の確率
+    ax.axvline(
+        x=mu, 
+        color='black', linewidth=1.0, linestyle='dashed', 
+        zorder=11
+    ) # 期待値の位置
+    ax.hlines(
+        y=0.0, xmin=mu-sigma, xmax=mu+sigma, 
+        color='black', linewidth=1.0, 
+        zorder=11
+    ) # 標準偏差の範囲
+    for label_x, label_str in zip([mu-sigma, mu, mu+sigma], ['$-\sigma$', '$\mu$', '$+\sigma$']):
+        ax.text(
+            x=label_x, y=0.0, 
+            s=label_str, ha='center', va='top', 
+            size=10, 
+            zorder=12
+        ) # 統計量のラベル
+    ax.plot(
+        x_vec, prob_vec, 
+        color='#00A968', linewidth=1.0, 
+        label='poisson', 
+        zorder=13
+    ) # ベルヌーイ分布の確率
+    ax.scatter(
+        x=x_vec, y=prob_vec, 
+        color='#00A968', s=30, 
+        zorder=13
+    ) # ベルヌーイ分布の確率
+    ax.plot(
+        norm_x_vec, norm_dens_vec, 
+        color='red', linewidth=1.0, linestyle='dashed', 
+        label='gaussian', 
+        zorder=14
+    ) # ガウス分布の確率密度
+    ax.text(
+        x=moment_loc_x, y=moment_loc_y, 
+        s=moment_lbl, transform=ax.transAxes, ha='left', va='top', 
+        bbox=dict(facecolor='white', alpha=0.8, edgecolor='black', linewidth=0.5), 
+        size = 10, 
+        zorder=100
+    ) # モーメントのラベル
+    ax.set_xticks(ticks=x_vec) # x軸目盛
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('probability, density')
+    ax.set_title(param_lbl, loc='left')
+    ax.legend(title='distribution', prop={'size': 8}, loc='upper right')
+    ax.grid(zorder=0)
+    ax.set_xlim(xmin=x_min-0.5, xmax=x_max+0.5) # (垂直線がはみ出すときの対策用)
+    ax.set_ylim(ymin=y_min, ymax=y_max) # (ラベルの表示用)
+
+# 動画を作成
+anim = FuncAnimation(
+    fig=fig, func=update, init_func=init, 
+    frames=frame_num, interval=100
+)
+
+# 動画を書出
+anim.save(
+    filename='../../figure/bernoulli/parameter/moment_phi.mp4', 
+    progress_callback=lambda i, n: print(f'\rframe: {i+1} / {n}', end='', flush=True)
+)
+
+
+# %%
+
 
