60c6eb7669b2d94d91239b8710067c31cf8dc314
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2026-01-15 12:45:33 +0900

    全体をアプデ

--- code/gamma/random_number.py
+++ code/gamma/random_number.py
@@ -8,139 +8,398 @@
 
 # ライブラリの読込 ---------------------------------------------------------------
 
-# 利用するライブラリ
+# ライブラリを読込
 import numpy as np
-from scipy.stats import gamma, norm, poisson # ガンマ分布, 1次元ガウス分布, ポアソン分布
-import scipy.special as sp # ガンマ関数, 対数ガンマ関数
+from scipy.stats import gamma
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+
 # %%
 
-### 乱数の生成
+# サンプルサイズの影響 -----------------------------------------------------------
 
-## 乱数の生成
+### パラメータの設定 -----
 
 # パラメータを指定
 a = 2.0
 b = 2.0
 
-# データ数(サンプルサイズ)を指定
+
+# %%
+
+### 乱数の生成 -----
+
+# サンプルサイズを指定
 N = 1000
 
-# ガンマ分布に従う乱数を生成
-lambda_n = np.random.gamma(shape=a, scale=1.0 / b, size=N)
+# ガンマ分布の乱数を生成
+lambda_n = np.random.gamma(shape=a, scale=1.0/b, size=N)
 
-# 作図用のlambdaの点を作成
-lambda_vals = np.linspace(start=0.0, stop=np.max(lambda_n) + 1.0, num=250)
 
-# ガンマ分布を計算
-density = gamma.pdf(x=lambda_vals, a=a, scale=1.0 / b)
+# %%
 
-#%%
+### 変数の設定 -----
 
-## 乱数の可視化
+# λ軸の範囲を設定
+u = 5.0
+lambda_min = 0.0
+lambda_max = np.max(lambda_n)
+#lambda_max = np.max(lambda_n[:frame_num]) # 「1サンプルずつ」の場合
+#lambda_max = np.max(lambda_n[:(smp_per_frame*frame_num)]) # 「複数サンプルずつ」の場合
+lambda_max  = np.ceil(lambda_max /u)*u # u単位で切り上げ
+print('λ size:', lambda_min, lambda_max)
 
-# サンプルのヒストグラム(頻度)を作成
-plt.figure(figsize=(12, 9)) # 図の設定
-plt.hist(x=lambda_n, bins=30, range=(lambda_vals.min(), lambda_vals.max()), color='#00A968') # ヒストグラム
-plt.xlabel('$\lambda$') # x軸ラベル
-plt.ylabel('density') # y軸ラベル
-plt.suptitle('Gamma Distribution', fontsize=20) # 全体のタイトル
-plt.title('$a=' + str(a) + ', b=' + str(b) + ', N=' + str(N) + '$', loc='left') # タイトル
-plt.grid() # グリッド線
-plt.ylim(ymin=-0.01) # y軸の表示範囲
-plt.show() # 描画
+# λ軸の値を作成
+lambda_vec = np.linspace(start=lambda_min, stop=lambda_max, num=1001)
 
-# サンプルのヒストグラム(密度)を作成
-plt.figure(figsize=(12, 9)) # 図の設定
-plt.hist(x=lambda_n, bins=30, range=(lambda_vals.min(), lambda_vals.max()), density=True, color='#00A968') # ヒストグラム
-plt.plot(lambda_vals, density, color='green', linestyle='--') # 元の分布
-plt.xlabel('$\lambda$') # x軸ラベル
-plt.ylabel('density') # y軸ラベル
-plt.suptitle('Gamma Distribution', fontsize=20) # 全体のタイトル
-plt.title('$a=' + str(a) + ', b=' + str(b) + ', N=' + str(N) + '$', loc='left') # タイトル
-plt.grid() # グリッド線
-plt.ylim(ymin=-0.01) # y軸の表示範囲
-plt.show() # 描画
 
-#%%
+# %%
 
-## アニメーションによる可視化:(頻度)
+### 分布の計算 -----
 
-# フレーム数を指定
-N_frame = 100
+# ガンマ分布の確率密度を計算
+dens_vec = gamma.pdf(x=lambda_vec, a=a, scale=1.0/b)
 
-# 図を初期化
-fig = plt.figure(figsize=(12, 9)) # 図の設定
-fig.suptitle('Gaussian Distribution', fontsize=20) # 全体のタイトル
 
-# y軸(頻度)の最大値を設定
-freq_max = np.max(
-    np.histogram(a=lambda_n[:N_frame], bins=30, range=(lambda_vals.min(), lambda_vals.max()))[0], 
-) + 1.0
+# %%
 
-# 作図処理を関数として定義
-def update(n):
-    # 前フレームのグラフを初期化
-    plt.cla()
-    
-    # サンプルのヒストグラムを作成
-    plt.hist(x=lambda_n[:(n+1)], bins=30, range=(lambda_vals.min(), lambda_vals.max()), color='#00A968', zorder=1) # ヒストグラム
-    plt.scatter(x=lambda_n[n], y=0.0, s=100, color='orange', zorder=2) # サンプル
-    plt.xlabel('$\lambda$') # x軸ラベル
-    plt.ylabel('frequency') # y軸ラベル
-    plt.suptitle('Gamma Distribution', fontsize=20) # 全体のタイトル
-    plt.title('$a=' + str(a) + ', b=' + str(b) + ', N=' + str(n + 1) + '$', loc='left') # タイトル
-    plt.grid() # グリッド線
-    plt.ylim(ymin=-0.5, ymax=freq_max) # y軸の表示範囲
+### 乱数の可視化 -----
 
-# gif画像を作成
-anime_freq = FuncAnimation(fig, update, frames=N_frame, interval=100)
+# 階級数を指定
+bin_num = 40
 
-# gif画像を保存
-anime_freq.save('ProbabilityDistribution/Gamma_freq.gif')
+# 階級幅を設定
+bin_size = (lambda_max - lambda_min) / bin_num
+print('bar size:', bin_size)
 
-#%%
 
-## アニメーションによる可視化:(密度)
+# %%
+
+#### 1サンプルずつ集計 -----
 
 # フレーム数を指定
-N_frame = 100
+frame_num = 300
+
+
+# 度数軸の範囲を設定
+u = 5.0
+counts, _ = np.histogram(
+    a=lambda_n[:frame_num], bins=bin_num, range=(lambda_min, lambda_max)
+) # 対象を抽出して集計
+freq_max = np.max(counts)
+freq_max = np.ceil(freq_max /u)*u # u単位で切り上げ
+print('Nx size:', freq_max)
+
+# 密度軸の範囲を設定
+u = 0.5
+counts, _ = np.histogram(
+    a=lambda_n[:frame_num], bins=bin_num, range=(lambda_min, lambda_max), 
+    density=True
+) # 対象を抽出して集計
+dens_max = np.max(counts)
+dens_max = np.ceil(dens_max /u)*u # u単位で切り上げ
+print('p(x) size:', dens_max)
+
+
+# %%
+
+##### 度数の作図 -----
 
 # 図を初期化
-fig = plt.figure(figsize=(12, 9)) # 図の設定
-fig.suptitle('Gaussian Distribution', fontsize=20) # 全体のタイトル
+fig, ax = plt.subplots(figsize=(9, 6), dpi=100, facecolor='white')
+fig.suptitle('Gamma distribution', fontsize=20)
 
-# y軸(確率密度)の最大値を設定
-dens_max = np.max(
-    np.hstack([
-        np.histogram(a=lambda_n[:N_frame], bins=30, range=(lambda_vals.min(), lambda_vals.max()), density=True)[0], 
-        density
-    ])
-) + 0.1
+# 初期化処理を定義
+def init():
+    pass
 
-# 作図処理を関数として定義
-def update(n):
+# 作図処理を定義
+def update(i):
+
     # 前フレームのグラフを初期化
-    plt.cla()
+    ax.cla()
+
+    # 値を設定
+    n = i + 1 # サンプル数
+
+    # ラベル用の文字列を作成
+    param_lbl = f'$N = {n}, a = {a:.2g}, b = {b:.2g}$'
     
-    # サンプルのヒストグラムを作成
-    plt.hist(x=lambda_n[:(n+1)], bins=30, range=(lambda_vals.min(), lambda_vals.max()), density=True, color='#00A968', zorder=1) # ヒストグラム
-    plt.plot(lambda_vals, density, color='green', linestyle='--', zorder=2) # 元の分布
-    plt.scatter(x=lambda_n[n], y=0.0, s=100, color='orange', zorder=3) # サンプル
-    plt.xlabel('$\lambda$') # x軸ラベル
-    plt.ylabel('density') # y軸ラベル
-    plt.suptitle('Gamma Distribution', fontsize=20) # 全体のタイトル
-    plt.title('$a=' + str(a) + ', b=' + str(b) + ', N=' + str(n + 1) + '$', loc='left') # タイトル
-    plt.grid() # グリッド線
-    plt.ylim(ymin=-0.01, ymax=dens_max) # y軸の表示範囲
+    # サンプルの度数を描画
+    ax.hist(
+        x=lambda_n[:n], 
+        bins=bin_num, range=(lambda_min, lambda_max), 
+        color='#00A968', 
+        zorder=10
+    ) # 度数
+    ax.scatter(
+        x=lambda_n[:i], y=np.zeros(i), 
+        color='orange', alpha=0.33, s=10, clip_on=False, 
+        zorder=11
+    ) # 過去サンプル
+    ax.scatter(
+        x=lambda_n[i], y=0.0, 
+        color='orange', s=50, clip_on=False, 
+        zorder=12
+    ) # 新サンプル
+    ax.set_xlabel('$\lambda$')
+    ax.set_ylabel('frequency')
+    ax.set_title(param_lbl, loc='left')
+    ax.grid()
+    ax.set_ylim(ymin=0.0, ymax=freq_max) # 描画範囲を固定
 
-# gif画像を作成
-anime_freq = FuncAnimation(fig, update, frames=N_frame, interval=100)
+# 動画を作成
+anim = FuncAnimation(
+    fig=fig, func=update, init_func=init, 
+    frames=frame_num, interval=100
+)
 
-# gif画像を保存
-anime_freq.save('ProbabilityDistribution/Gamma_prop.gif')
+# 動画を書出
+anim.save(
+    filename='../../figure/gamma/random_number/freq_1smp.mp4', 
+    progress_callback=lambda i, n: print(f'\rframe: {i+1} / {n}', end='', flush=True)
+)
 
-#%%
+
+# %%
+
+##### 密度の作図 -----
+
+# 図を初期化
+fig, ax = plt.subplots(figsize=(9, 6), dpi=100, facecolor='white')
+fig.suptitle('Gamma distribution', fontsize=20)
+ax2 = ax.twinx() # 第2軸の設定用
+
+# 初期化処理を定義
+def init():
+    pass
+
+# 作図処理を定義
+def update(i):
+
+    # 前フレームのグラフを初期化
+    ax.cla()
+    ax2.cla()
+
+    # 値を設定
+    n = i + 1 # サンプル数
+
+    # ラベル用の文字列を作成
+    param_lbl = f'$N = {n}, a = {a:.2g}, b = {b:.2g}$'
+
+    # サンプルの密度を描画
+    ax.hist(
+        x=lambda_n[:n], 
+        bins=bin_num, range=(lambda_min, lambda_max), density=True, 
+        color='#00A968', alpha=0.5, 
+        label='random number', zorder=10
+    ) # 密度
+    ax.plot(
+        lambda_vec, dens_vec, 
+        color='green', linewidth=1.0, linestyle='--', 
+        label='generator', zorder=11
+    ) # 確率密度
+    ax.scatter(
+        x=lambda_n[:(n-1)], y=np.zeros(n-1), 
+        color='orange', alpha=0.33, s=10, clip_on=False, 
+        zorder=12
+    ) # 過去サンプル
+    ax.scatter(
+        x=lambda_n[n-1], y=0.0, 
+        color='orange', s=50, clip_on=False, 
+        zorder=13
+    ) # 新サンプル
+    ax.set_xlabel('$\lambda$')
+    ax.set_ylabel('density')
+    ax.set_title(param_lbl, loc='left')
+    ax.legend(title='distribution', loc='upper right')
+    ax.grid()
+    ax.set_ylim(ymin=0.0, ymax=dens_max) # (目盛の共通化用)
+
+    # 度数軸を設定
+    freq_max  = dens_max * bin_size * n
+    dens_vals = ax.get_yticks()          # 密度軸目盛を取得
+    freq_vals = dens_vals * bin_size * n # 度数軸目盛に変換
+
+    # 第2軸を描画
+    ax2.set_yticks(ticks=freq_vals, labels=[f'{y:.1f}' for y in freq_vals]) # 度数軸目盛
+    ax2.set_ylabel('frequency')
+    ax2.yaxis.set_label_position(position='right') # (ラベルの表示位置が初期化される対策)
+    ax2.set_ylim(ymin=0.0, ymax=freq_max) # (目盛の共通化用)
+
+# 動画を作成
+anim = FuncAnimation(
+    fig=fig, func=update, init_func=init, 
+    frames=frame_num, interval=100
+)
+
+# 動画を書出
+anim.save(
+    filename='../../figure/gamma/random_number/dens_1smp.mp4', 
+    progress_callback=lambda i, n: print(f'\rframe: {i+1} / {n}', end='', flush=True)
+)
+
+
+# %%
+
+#### 複数サンプルずつ集計 -----
+
+# フレーム数を指定
+frame_num = 300
+
+# 1フレーム当たりのサンプル数を設定
+smp_per_frame = N // frame_num
+
+
+# 度数軸の範囲を設定
+u = 5.0
+counts, _ = np.histogram(
+    a=lambda_n[:(smp_per_frame*frame_num)], bins=bin_num, range=(lambda_min, lambda_max)
+) # 対象を抽出して集計
+freq_max = np.max(counts)
+freq_max = np.ceil(freq_max /u)*u # u単位で切り上げ
+print('Nx size:', freq_max)
+
+# 密度軸の範囲を設定
+u = 0.5
+counts, _ = np.histogram(
+    a=lambda_n[:(smp_per_frame*frame_num)], bins=bin_num, range=(lambda_min, lambda_max), 
+    density=True
+) # 対象を抽出して集計
+dens_max = np.max(counts)
+dens_max = np.ceil(dens_max /u)*u # u単位で切り上げ
+dens_max = 0.3
+print('p(x) size:', dens_max)
+
+
+# %%
+
+##### 度数の作図 -----
+
+# 図を初期化
+fig, ax = plt.subplots(figsize=(9, 6), dpi=100, facecolor='white')
+fig.suptitle('Gamma distribution', fontsize=20)
+
+# 初期化処理を定義
+def init():
+    pass
+
+# 作図処理を定義
+def update(i):
+
+    # 前フレームのグラフを初期化
+    ax.cla()
+
+    # 値を設定
+    n = smp_per_frame * (i+1) # サンプル数
+
+    # ラベル用の文字列を作成
+    param_lbl = f'$N = {n}, a = {a:.2g}, b = {b:.2g}$'
+    
+    # サンプルの度数を描画
+    ax.hist(
+        x=lambda_n[:n], 
+        bins=bin_num, range=(lambda_min, lambda_max), 
+        color='#00A968', 
+        zorder=10
+    ) # 度数
+    ax.scatter(
+        x=lambda_n[:n], y=np.zeros(n), 
+        color='orange', alpha=0.5, s=10, clip_on=False, 
+        zorder=11
+    ) # サンプル
+    ax.set_xlabel('$\lambda$')
+    ax.set_ylabel('frequency')
+    ax.set_title(param_lbl, loc='left')
+    ax.grid()
+    #ax.set_ylim(ymin=0.0, ymax=freq_max) # 描画範囲を固定
+
+# 動画を作成
+anim = FuncAnimation(
+    fig=fig, func=update, init_func=init, 
+    frames=frame_num, interval=100
+)
+
+# 動画を書出
+anim.save(
+    filename='../../figure/gamma/random_number/freq_nsmp.mp4', 
+    progress_callback=lambda i, n: print(f'\rframe: {i+1} / {n}', end='', flush=True)
+)
+
+
+# %%
+
+##### 密度の作図 -----
+
+# 図を初期化
+fig, ax = plt.subplots(figsize=(9, 6), dpi=100, facecolor='white')
+fig.suptitle('Gamma distribution', fontsize=20)
+ax2 = ax.twinx() # 第2軸の設定用
+
+# 初期化処理を定義
+def init():
+    pass
+
+# 作図処理を定義
+def update(n):
+
+    # 前フレームのグラフを初期化
+    ax.cla()
+    ax2.cla()
+    
+    # 値を調整
+    n = smp_per_frame * (n+1)
+
+    # ラベル用の文字列を作成
+    param_lbl = f'$N = {n}, a = {a:.2g}, b = {b:.2g}$'
+
+    # サンプルの密度を描画
+    ax.hist(
+        x=lambda_n[:n], 
+        bins=bin_num, range=(lambda_min, lambda_max), density=True, 
+        color='#00A968', alpha=0.5, 
+        label='random number', zorder=0
+    ) # 密度
+    ax.plot(
+        lambda_vec, dens_vec, 
+        color='green', linewidth=1.0, linestyle='--', 
+        label='generator', zorder=1
+    ) # 確率密度
+    ax.scatter(
+        x=lambda_n[:n], y=np.zeros(n), 
+        color='orange', alpha=0.5, s=10, clip_on=False, zorder=2
+    ) # サンプル
+    ax.grid()
+    ax.set_xlabel('$\lambda$')
+    ax.set_ylabel('density')
+    ax.set_title(param_lbl, loc='left')
+    ax.set_ylim(ymin=0.0, ymax=dens_max) # 描画範囲を固定 # (目盛の共通化用)
+
+    # 度数軸を設定
+    freq_max  = dens_max * bin_size * n
+    dens_vals = ax.get_yticks()          # 密度軸目盛を取得
+    freq_vals = dens_vals * bin_size * n # 度数軸目盛に変換
+
+    # 第2軸を描画
+    ax2.set_yticks(ticks=freq_vals, labels=[f'{y:.1f}' for y in freq_vals]) # 度数軸目盛
+    ax2.set_ylabel('frequency')
+    ax2.yaxis.set_label_position(position='right') # (ラベルの表示位置が初期化される対策)
+    ax2.set_ylim(ymin=0.0, ymax=freq_max) # (目盛の共通化用)
+
+# 動画を作成
+anim = FuncAnimation(
+    fig=fig, func=update, init_func=init, 
+    frames=frame_num, interval=100
+)
+
+# 動画を書出
+anim.save(
+    filename='../../figure/gamma/random_number/dens_nsmp.mp4', 
+    progress_callback=lambda i, n: print(f'\rframe: {i+1} / {n}', end='', flush=True)
+)
+
+
+# %%
+
 
